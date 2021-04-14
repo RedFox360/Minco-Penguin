9d157c0c58699acdb3de20867660abc74c14c964
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-14 08:05:28 -0700

    heroku update

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-import { Client } from 'discord.js';
+const { Client } = require('discord.js');
 /** @param {Client} client */
 module.exports = (client) => {
     console.log(`${client.user.tag} is online!`);
