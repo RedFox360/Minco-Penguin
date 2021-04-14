6a78239a26b8fd5a3cde894cbc6410b32ab472c9
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-14 14:30:21 -0700

    heroku update

--- commands/server/code.js
+++ commands/server/code.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, version } = require("discord.js");
 module.exports = {
     name: 'code',
     description: 'sends info about the code',
@@ -8,7 +8,7 @@ module.exports = {
             .setColor('70E5FF')
             .addFields(
                 { name: 'Node Version', value: 'v14.16.1' },
-                { name: 'Discord.js Version', value: 'v12.5.1' },
+                { name: 'Discord.js Version', value: version },
                 { name: 'Heroku', value: 'Heroku free account, view the server [here](https://minco-penguin.herokuapp.com/)' },
                 { name: 'Packages used', value: 'discord.js, ms, dotenv, express, fs, mongoose' },
             );
