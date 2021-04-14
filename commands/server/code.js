5b9b009af6d38fe31a496a3a75e5035dda9931f1
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-14 14:31:23 -0700

    heroku update

--- commands/server/code.js
+++ commands/server/code.js
@@ -8,7 +8,7 @@ module.exports = {
             .setColor('70E5FF')
             .addFields(
                 { name: 'Node Version', value: 'v14.16.1' },
-                { name: 'Discord.js Version', value: version },
+                { name: 'Discord.js Version', value: 'v' + version },
                 { name: 'Heroku', value: 'Heroku free account, view the server [here](https://minco-penguin.herokuapp.com/)' },
                 { name: 'Packages used', value: 'discord.js, ms, dotenv, express, fs, mongoose' },
             );
