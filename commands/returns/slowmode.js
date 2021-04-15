04d6f729037c4992e26506b92ba73ac7a73d165a
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-15 14:20:19 -0700

    heroku update

--- commands/returns/slowmode.js
+++ commands/returns/slowmode.js
@@ -1,15 +1,18 @@
-const { MessageEmbed } = require("discord.js");
+const ms = require('ms');
+const { MessageEmbed, Message } = require("discord.js");
 module.exports = {
     name: 'slowmode',
     description: "[MANAGE CHANNELS] Changes slowmode",
     usage: '!slowmode <number>',
+    /** @param {Message} message */
     execute(message, args) {
         if (message.member.hasPermission("MANAGE_CHANNELS")) {
             if (!args.length) return "You didn't provide any arguments.";
             var slowmode;
             if (args[0] == 'off') slowmode = 0;
-            else slowmode = parseInt(args[0]);
+            else slowmode = ms(args.join(' ')) / 1000;
             if (isNaN(slowmode)) return "Enter a valid number";
+            if (slowmode > 21600) return "Please enter an amount less than or equal to 6 hours"
             message.channel.setRateLimitPerUser(slowmode, null);
             let confirmEmbed = new MessageEmbed()
                 .setColor('#7E78D2')
