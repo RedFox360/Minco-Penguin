9c23ac81e3fc943114f7fc6aaab4c31f343c1613
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-15 12:30:04 -0700

    heroku update

---
+++ commands/fun/joo.js
@@ -0,0 +1,5 @@
+module.exports = {
+    name: 'joo',
+    description: 'information on the nonsense word **joo**',
+    execute: () => 'Important Nonsense Word: **JOO** :eyes: \njoo = ok\njoo = no\njoo = yes\njoo = fine\njoo = good\njoo = do that in the spamming channel\njoo = please\njoo = keep it a secret\njoo = don\'t tell\njoo = hi\njoo = bye\njoo = yee\njoo = yeet\njoo = focus on class';
+}
