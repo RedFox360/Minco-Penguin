58f2cac128fc0b1464aacaf6087959e177ecd028
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-17 15:55:36 -0700

    Revert "heroku update"
    This reverts commit 3c7f28657a3ed20bacf235499266744ccc786c85.

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import * as Discord from "discord.js";
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 import * as mongoose from "mongoose";
 
-client.commands = new Discord.Collection();
+export const commands = new Discord.Collection();
 
 import command_handler from "./handlers/command_handler";
 import event_handler from "./handlers/event_handler";
