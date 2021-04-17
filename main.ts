3c7f28657a3ed20bacf235499266744ccc786c85
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-17 15:19:31 -0700

    heroku update

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import * as Discord from "discord.js";
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 import * as mongoose from "mongoose";
 
-export const commands = new Discord.Collection();
+client.commands = new Discord.Collection();
 
 import command_handler from "./handlers/command_handler";
 import event_handler from "./handlers/event_handler";
