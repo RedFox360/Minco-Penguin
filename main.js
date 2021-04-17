0c18f6c777a7d1cc00f53c252dc247c7f353f9da
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-17 14:57:55 -0700

    heroku update

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict";
+console.log(require);
 exports.__esModule = true;
 exports.commands = void 0;
 var Discord = require("discord.js");
@@ -6,16 +7,18 @@ require("dotenv").config();
 var client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 var mongoose = require("mongoose");
 exports.commands = new Discord.Collection();
+
 require("./handlers/command_handler")(client);
 require("./handlers/event_handler")(client);
 mongoose
-    .connect(process.env.SRV, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
-    .then(function () {
-    console.log("Connected to the database!");
-})["catch"](console.error);
+	.connect(process.env.SRV, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then(function () {
+		console.log("Connected to the database!");
+	})
+	["catch"](console.error);
 require("./server")();
 client.login(process.env.TOKEN);
