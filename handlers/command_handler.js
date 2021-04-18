62b196b5502b162ba535fcbbd9ccedd49d401237
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-18 12:13:09 -0700

    heroku update

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
+const { readdirSync } = require("fs");
 module.exports = (client) => {
-	const categories = fs.readdirSync("./commands/").filter((file) => !file.endsWith(".DS_Store"));
+	const categories = readdirSync("./commands/").filter((file) => !file.endsWith(".DS_Store"));
 	for (const category of categories) {
-		const commandFiles = fs.readdirSync(`./commands/${category}`).filter((file) => file.endsWith(".js"));
+		const commandFiles = readdirSync(`./commands/${category}`).filter((file) => file.endsWith(".js"));
 		for (const file of commandFiles) {
 			const command = require(`../commands/${category}/${file}`);
 			client.commands.set(file.split(".")[0], command);
