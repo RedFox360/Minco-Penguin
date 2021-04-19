607bb00ca76d7daab4560ef1209f180794bb0296
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-19 14:14:27 -0700

    heroku update

--- events/guildBanRemove.js
+++ events/guildBanRemove.js
@@ -4,11 +4,11 @@ const Discord = require("discord.js");
  * @param {Discord.User} user
  */
 module.exports = (_, guild, user) => {
-  let banEmbed = new Discord.MessageEmbed()
-    .setColor("F75853") // orange
-    .setTitle("Banned")
-    .setDescription(`${user.tag} was unbanned from ${guild.name}.`);
+	let banEmbed = new Discord.MessageEmbed()
+		.setColor("F75853") // orange
+		.setTitle("Banned")
+		.setDescription(`${user.tag} was unbanned from ${guild.name}.`);
 
-  guild.systemChannel.send(banEmbed);
-  user.send(`${user.tag}, you were banned from ${guild.name}.`);
+	guild.systemChannel.send(banEmbed);
+	user.send(`${user.tag}, you were banned from ${guild.name}.`);
 };
