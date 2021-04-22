3ce721570e4533bf40f4fa9dc75d24ac739911ef
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-22 10:57:32 -0700

    heroku update

--- commands/returns/solve.js
+++ commands/returns/solve.js
@@ -3,14 +3,13 @@ module.exports = {
 	description: "Solve a math problem\nOperations: +, -, *, /, ^, sqrt, cbrt",
 	aliases: ["s"],
 	usage: "!solve <Math Equation>",
-	execute(message, args, cmd, client, profileData) {
+	execute(message, args) {
 		if (!args.length)
 			return message.channel.send(
 				"Enter valid arguments\nValid usage: !s 2 + 2 * 3 (spaces between numbers and operations required)\nOperations: +: add, - (subtract), *: multiply, /: divide, ^: exponents, sqrt(number): Square Root, cbrt(number): Cube Root"
 			);
 		const equation = simplify(args.join(" ")).toString().split(" ");
 		const result = calculate(equation[0], equation[2], equation[1]);
-		if (result == "∞") result = "undefined";
 		message.channel.send(`${args.join(" ")} = __${result.toLocaleString()}__`);
 	},
 };
