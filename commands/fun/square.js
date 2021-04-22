119bd5fea284884aed312a0eb490183214c5b52c
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-22 16:32:43 -0700

    heroku update

---
+++ commands/fun/square.js
@@ -0,0 +1,14 @@
+const { isInteger } = require("mathjs");
+
+module.exports = {
+	description: "Check if a number is a perfect square",
+	usage: "!square <number>",
+	aliases: ["perfect-square", "psq"],
+	execute(_, args) {
+		let number = parseInt(args[0]);
+		if (isNaN(number)) return "Enter a valid number";
+		const isSquare = isInteger(Math.sqrt(number));
+		if (isSquare) return `${number} is a perfect square`;
+		else return `${number} is not a perfect square`;
+	},
+};
