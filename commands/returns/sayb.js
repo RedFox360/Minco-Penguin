9e4fbcc4755994eb7628eb113afb22cc1a2dd274
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-23 09:02:26 -0700

    heroku update

---
+++ commands/returns/sayb.js
@@ -0,0 +1,13 @@
+const { Message } = require("discord.js");
+module.exports = {
+	description: "Say with blob bot!",
+	usage: "!sayb <words>",
+	/** @param {Message} message */
+	execute(message, args) {
+		if (!args.length) return "You didn't provide any arguments.";
+		message.delete();
+		message.channel.send(`+say ${args.join(" ")}`).then((msg) => {
+			msg.delete();
+		});
+	},
+};
