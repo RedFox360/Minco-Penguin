f8c02dbcfd275775e9fe28f6c9f4cf276a9edbf4
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-25 11:29:17 -0700

    heroku update

--- commands/returns/pollabove.js
+++ commands/returns/pollabove.js
@@ -3,27 +3,22 @@ module.exports = {
 	description: "Adds poll reactions to the message above",
 	aliases: ["spollabove", "polla", "spolla"],
 	/** @param {Message} message */
-	execute(message, _, cmd) {
+	async execute(message, _, cmd) {
 		message.delete();
 		var react = ["👍", "👎"];
 		if (cmd === "spolla" || cmd === "spollabove") react.push("🤷");
-		message.channel.messages.fetch({ limit: 1 }).then(async (messages) => {
-			let lastMessage = messages.first();
-
-			if (lastMessage.author.bot) return;
-
-			try {
-				react.forEach(async (reaction) => {
-					try {
-						await lastMessage.react(reaction);
-					} catch (err) {
-						console.error(err);
-						return;
-					}
-				});
-			} catch (err) {
-				console.error(err);
-			}
-		});
+		const lastMessage = message.channel.lastMessage;
+		try {
+			react.forEach(async (reaction) => {
+				try {
+					await lastMessage.react(reaction);
+				} catch (err) {
+					console.error(err);
+					return;
+				}
+			});
+		} catch (err) {
+			console.error(err);
+		}
 	},
 };
