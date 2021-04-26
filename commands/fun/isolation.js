786a919a2e860a2d328cc348f26409452fa16fab
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-26 07:56:42 -0700

    heroku update

---
+++ commands/fun/isolation.js
@@ -0,0 +1,16 @@
+const { Message } = require("discord.js");
+module.exports = {
+	description: "[Carrel Crew] Unlocks/locks the isolation channel",
+	usage: "!isolation",
+	/** @param {Message} message */
+	execute(message) {
+		if (message.guild.id != "785642761814671381") return "This command can only be used in Carrel Crew";
+		let isolationRole = message.guild.roles.cache.get((role) => role.name === "Isolation");
+		if (message.member.roles.cache.has(isolationRole.id)) {
+			message.member.roles.cache.delete(isolationRole.id);
+			return `Isolation locked for user ${message.author.toString()}`;
+		}
+		message.member.roles.add(isolationRole);
+		return `Isolation unlocked for user ${message.author.toString()}`;
+	},
+};
