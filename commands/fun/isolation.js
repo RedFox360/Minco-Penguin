fe9fd59fec07f4b70e6028e4e2ed17c95c19011e
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-26 08:02:01 -0700

    heroku update

--- commands/fun/isolation.js
+++ commands/fun/isolation.js
@@ -7,7 +7,7 @@ module.exports = {
 		if (message.guild.id != "785642761814671381") return "This command can only be used in Carrel Crew";
 		let isolationRole = message.guild.roles.cache.get("836253125543329843");
 		if (message.member.roles.cache.has(isolationRole.id)) {
-			message.member.roles.cache.delete(isolationRole.id);
+			message.member.roles.remove(isolationRole);
 			return `Isolation locked for user ${message.author.toString()}`;
 		}
 		message.member.roles.add(isolationRole);
