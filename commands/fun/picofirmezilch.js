94aa2a063cf7fa75f58685f5ba3b70e37cef4ba1
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-26 17:28:53 -0700

    heroku update

--- commands/fun/picofirmezilch.js
+++ commands/fun/picofirmezilch.js
@@ -1,7 +1,6 @@
-const games = new Map();
 const { MessageCollector } = require("discord.js-collector");
 const { randomInt } = require("mathjs");
-const { Message } = require("discord.js");
+const { Message, MessageEmbed } = require("discord.js");
 module.exports = {
 	description: "Play pico firme zilch in Discord\n4 digit number",
 	aliases: ["pfz"],
@@ -10,6 +9,11 @@ module.exports = {
 		var number = randomInt(1000, 10000);
 		var digits = number.toString().split("");
 		var userDigits = ["_", "_", "_", "_"];
+		const updateEmbed = new MessageEmbed()
+			.setAuthor(message.member.nickname || message.author.username)
+			.setColor("Pico Firme Zilch")
+			.setDescription("Guessed Digits: " + userDigits.join(""));
+		const gameMsg = await message.channel.send(updateEmbed);
 		var picos = 0;
 		while (true) {
 			const placeMessage = await message.channel.send("Choose a place in the number (1-4): ");
@@ -25,6 +29,10 @@ module.exports = {
 				message.channel.send("Terminating...");
 				return;
 			}
+			setTimeout(() => {
+				placeMessage.delete();
+				response.delete();
+			}, 5000);
 			let number_at_place = digits[place - 1];
 
 			const digitMessage = await message.channel.send("Guess the digit: ");
@@ -34,18 +42,21 @@ module.exports = {
 				user: message.author.id,
 			});
 			let guess = response2.content;
-
+			setTimeout(() => {
+				digitMessage.delete();
+				response2.delete();
+			}, 5000);
 			if (number_at_place == guess) {
 				message.channel.send("Pico");
 				picos += 1;
 				userDigits[place - 1] = number_at_place;
-			} else if (guess in digits) {
+			} else if (digits.includes(guess) && !userDigits.includes(guess)) {
 				message.channel.send("Firme");
 			} else {
 				message.channel.send("Zilch");
 			}
-			let userDigitsStr = userDigits.join("");
-			message.channel.send("Current: " + userDigitsStr);
+			updateEmbed.setDescription("Guessed Digits: " + userDigits.join(""));
+			gameMsg.edit(updateEmbed);
 			if (picos == 4) {
 				message.channel.send("You won!");
 				break;
