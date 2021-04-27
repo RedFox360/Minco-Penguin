bd65fb7746b309cca6434b0ce0b73259e45bd8de
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-26 18:05:38 -0700

    heroku update

--- commands/fun/picofirmezilch.js
+++ commands/fun/picofirmezilch.js
@@ -6,6 +6,18 @@ module.exports = {
 	aliases: ["pfz"],
 	/** @param {Message} message */
 	async execute(message) {
+		const channel = await message.guild.channels.create(
+			message.member.nickname.toLowerCase() || message.author.username.toLowerCase() + "-pfz"
+		);
+		channel.setParent(message.channel.parent);
+		channel.updateOverwrite(message.guild.roles.everyone, {
+			VIEW_CHANNEL: false,
+		});
+		channel.updateOverwrite(message.author, {
+			VIEW_CHANNEL: true,
+			SEND_MESSAGES: true,
+		});
+		message.channel.send(`Pico Firme Zilch game created in <@${channel.id}>`);
 		var number = randomInt(1000, 10000);
 		var digits = number.toString().split("");
 		var userDigits = ["\\_", "\\_", "\\_", "\\_"];
@@ -17,15 +29,15 @@ module.exports = {
 			.setColor("BLUE")
 			.setThumbnail(message.author.avatarURL())
 			.setDescription("Guessed Digits: " + userDigits.join(""));
-		const gameMsg = await message.channel.send(updateEmbed);
+		const gameMsg = await channel.send(updateEmbed);
 		while (true) {
-			const placeMessage = await message.channel.send(message.author.toString() + ": Choose a place in the number (1-4): ");
+			const placeMessage = await channel.send(message.author.toString() + ": Choose a place in the number (1-4): ");
 			const response = await MessageCollector.asyncQuestion({
 				botMessage: placeMessage,
 				user: message.author.id,
 			});
 			let place;
-			if (isNaN(parseInt(response.content))) return message.channel.send(message.author.toString() + ": Terminating...");
+			if (isNaN(parseInt(response.content))) return channel.send(message.author.toString() + ": Terminating...");
 			place = parseInt(response.content);
 			setTimeout(() => {
 				placeMessage.delete();
@@ -33,30 +45,30 @@ module.exports = {
 			}, 2200);
 			let number_at_place = digits[place - 1];
 
-			const digitMessage = await message.channel.send(message.author.toString() + ": Guess the digit: ");
+			const digitMessage = await channel.send(message.author.toString() + ": Guess the digit: ");
 
 			const response2 = await MessageCollector.asyncQuestion({
 				botMessage: digitMessage,
 				user: message.author.id,
 			});
 			let guess = response2.content;
-			if (isNaN(parseInt(guess))) return message.channel.send(message.author.toString() + ": Terminating...");
+			if (isNaN(parseInt(guess))) return channel.send(message.author.toString() + ": Terminating...");
 			setTimeout(() => {
 				digitMessage.delete();
 				response2.delete();
 			}, 2200);
 			if (number_at_place == guess) {
-				message.channel.send("Pico").then((m) => {
+				channel.send("Pico").then((m) => {
 					setTimeout(() => m.delete(), 2200);
 				});
 				userDigits[place - 1] = number_at_place;
 			} else if (digits.includes(guess) && !userDigits.includes(guess)) {
-				message.channel.send("Firme").then((m) => {
+				channel.send("Firme").then((m) => {
 					setTimeout(() => m.delete(), 2200);
 				});
 				firmes.push(guess + " not at " + place);
 			} else {
-				message.channel.send("Zilch").then((m) => {
+				channel.send("Zilch").then((m) => {
 					setTimeout(() => m.delete(), 2200);
 				});
 				if (!zilches.includes(guess)) zilches.push(guess);
@@ -66,7 +78,9 @@ module.exports = {
 			);
 			gameMsg.edit(updateEmbed);
 			if (userDigits.every((user) => user !== "\\_")) {
-				message.channel.send("You won!");
+				channel.send("You won!");
+				channel.send("Deleting channel in 10 seconds...");
+				setTimeout(() => channel.delete(), 10000);
 				break;
 			}
 		}
