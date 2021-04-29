c73ce3e6b2e4222b4231a1263494a866923aaded
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-29 12:29:11 -0700

    heroku update

--- commands/fun/lie-detector.js
+++ commands/fun/lie-detector.js
@@ -16,6 +16,6 @@ module.exports = {
 		var color = randomL[randomInt].includes("lying") ? "🔴" : "🟢";
 		if (randomL[randomInt].includes("info")) color = "🟡";
 		let name = message.member.displayName || message.author.username;
-		message.channel.send(`${color} Lie Detector: ${randomL[randomInt]}\n${name} asks: *${args.join(" ")}*`);
+		message.channel.send(`${color}  Lie Detector: ${randomL[randomInt]}\n${name} asks: *${args.join(" ")}*`);
 	},
 };
