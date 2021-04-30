338b1129580461b137ca905b041b145d16b6cad0
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-04-30 07:22:09 -0700

    heroku update

--- commands/server/ban.js
+++ commands/server/ban.js
@@ -4,14 +4,13 @@ module.exports = {
 	usage: "!ban <@user>",
 	/** @param {Message} message */
 	execute(message) {
-		if (message.member.hasPermission("BAN_MEMBERS")) {
-			const mention = message.mentions.users.first();
-			if (!mention) return "Mention a valid user";
-			let memberTarget = message.guild.members.cache.get(mention.id);
-			if (memberTarget.hasPermission("MANAGE_SERVER") && memberTarget.hasPermission("BAN_MEMBERS"))
-				return message.channel.send("This person cannot be banned.");
-			memberTarget.ban();
-			return `${memberTarget.tag} was banned from the server.`;
-		}
+		if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You don't have the correct permissions to execute this command");
+		const mention = message.mentions.users.first();
+		if (!mention) return message.channel.send("Mention a valid user");
+		let memberTarget = message.guild.members.cache.get(mention.id);
+		if (memberTarget.hasPermission("MANAGE_SERVER") || memberTarget.hasPermission("BAN_MEMBERS"))
+			return message.channel.send("This person cannot be banned.");
+		await memberTarget.ban();
+		return `${memberTarget.tag} was banned from the server.`;
 	},
 };
--- commands/server/kick.js
+++ commands/server/kick.js
@@ -3,15 +3,15 @@ module.exports = {
 	description: "[ADMIN ONLY] Kick members",
 	usage: "!kick <@user>",
 	/** @param {Message} message */
-	execute(message) {
-		if (message.member.hasPermission("KICK_MEMBERS")) {
-			const mention = message.mentions.users.first();
-			if (!mention) return "Mention a valid user";
-			let memberTarget = message.guild.members.cache.get(mention.id);
-			if (memberTarget.hasPermission("MANAGE_SERVER") && memberTarget.hasPermission("KICK_MEMBERS"))
-				return message.channel.send("This person cannot be kicked.");
-			memberTarget.kick();
-			return `${memberTarget.tag} was kicked from the server.`;
-		}
+	async execute(message) {
+		if (!message.member.hasPermission("KICK_MEMBERS"))
+			return message.channel.send("You don't have the correct permissions to execute this command");
+		const mention = message.mentions.users.first();
+		if (!mention) return message.channel.send("Mention a valid user");
+		let memberTarget = message.guild.members.cache.get(mention.id);
+		if (memberTarget.hasPermission("MANAGE_SERVER") || memberTarget.hasPermission("KICK_MEMBERS"))
+			return message.channel.send("This person cannot be kicked.");
+		await memberTarget.kick();
+		return `${memberTarget.tag} was kicked from the server.`;
 	},
 };
