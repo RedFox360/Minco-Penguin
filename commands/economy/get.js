f3d09f4725f02b9d448af080b3f05d956beb3652
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-01 09:19:16 -0700

    heroku update

--- commands/economy/get.js
+++ commands/economy/get.js
@@ -1,6 +1,6 @@
 const { Message, MessageEmbed } = require("discord.js");
 const profileModel = require("../../models/profileSchema");
-const ms = require('ms');
+const ms = require("ms");
 module.exports = {
 	description: "Get Minco Dollars from a user (they have to react to approve)",
 	usage: "!get <@user> <amount>",
@@ -10,8 +10,8 @@ module.exports = {
 		if (!user) return message.channel.send("Mention a valid user");
 		const am = args[1];
 		if (!am) return message.channel.send("Enter an amount of Minco Dollars");
-        let amount = parseInt(am);
-        if (isNaN(amount)) return message.channel.send("Enter a valid number");
+		let amount = parseInt(am);
+		if (isNaN(amount)) return message.channel.send("Enter a valid number");
 		const userProfile = await profileModel.findOne({ userID: user.id });
 		if (amount > userProfile) return message.channel.send(`<@${user.id}> does not have ${amount} Minco Dollars.`);
 		const checkM = await message.channel.send(
@@ -26,20 +26,26 @@ module.exports = {
 		);
 		const filter = (reaction, user) => m.author.id == user.id && reaction.emoji.name === "✅";
 		const collector = checkM.createReactionCollector(filter, ms("3m"));
-        let onEnd = true;
-		collector.on("collect", (reaction, user) => {
-            onEnd = false;
-            message.channel.send("Request approved");
-            await profileModel.findOneAndUpdate({userID: user.id}, {
-                mincoDollars: -amount 
-            })
-            await profileModel.findOneAndUpdate({userID: message.author.id}, {
-                mincoDollars: amount 
-            })
-            message.channel.send("Transaction successful");
-        });
-        collector.on("end", (collected) =>{ 
-            if (onEnd) message.channel.send("Timed out...");
-        })
+		let onEnd = true;
+		collector.on("collect", async (reaction, user) => {
+			onEnd = false;
+			message.channel.send("Request approved");
+			await profileModel.findOneAndUpdate(
+				{ userID: user.id },
+				{
+					mincoDollars: -amount,
+				}
+			);
+			await profileModel.findOneAndUpdate(
+				{ userID: message.author.id },
+				{
+					mincoDollars: amount,
+				}
+			);
+			message.channel.send("Transaction successful");
+		});
+		collector.on("end", (collected) => {
+			if (onEnd) message.channel.send("Timed out...");
+		});
 	},
 };
