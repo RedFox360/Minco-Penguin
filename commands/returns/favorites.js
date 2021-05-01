481c29c448f2b98247d0d437d074b8c52d7a57c8
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-01 09:58:33 -0700

    heroku update

--- commands/returns/favorites.js
+++ commands/returns/favorites.js
@@ -4,9 +4,15 @@ module.exports = {
 	usage: "!favorites <@user>",
 	aliases: ["favs"],
 	async execute(message) {
+		var id;
 		const mention = message.mentions.users.first();
-		if (!mention) return message.channel.send("Mention a valid user");
-		let profile = await profileModel.findOne({ userID: mention.id });
+		if (mention) {
+			message.channel.send("Mention a valid user");
+			id = mention.id;
+		} else {
+			id = message.author.id;
+		}
+		let profile = await profileModel.findOne({ userID: id });
 		try {
 			message.channel.send("Animal: " + profile.favs.animal);
 			message.channel.send("Color: " + profile.favs.color);
