73ba5f40f4910abdea8790cc78f9c6772e413a4a
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-01 09:40:24 -0700

    heroku update

--- commands/fun/math.js
+++ commands/fun/math.js
@@ -3,21 +3,20 @@ const profileModel = require("../../models/profileSchema");
 const ms = require("ms");
 module.exports = {
 	description: "Sends a math question for you to solve",
-	usage: "!math <easy/medium/hard> <operation>",
+	usage: "!math <operation>",
 	cooldown: ms("15m") / 1000,
 	/** @param {Message} message */
 	async execute(message, args) {
 		var num1, num2, result;
-		if (!args.length)
-			return "Invalid usage. Correct usage: !math <easy/medium/hard> <operation>\n(Divions is not a permitted operation)";
+		if (!args.length) return "Invalid usage. Correct usage: !math <operation>\n(Divions is not a permitted operation)";
 		var oper;
 		if (args[0] == "add" || args[0] == "addition" || args[0] == "+") oper = "+";
 		else if (args[0] == "subtract" || args[0] == "minus" || args[0] == "-") oper = "-";
 		else if (args[0] == "multiply" || args[0] == "mult" || args[0] == "x" || args[0] == "*") oper = "*";
-		else return "Invalid usage: Correct usage: !math <easy/medium/hard> <operation>\n(Division is not a permitted operation)";
+		else return "Invalid usage: Correct usage: !math <operation>\n(Division is not a permitted operation)";
 		if (oper == "*") {
-			num1 = random(40, 60);
-			num2 = random(30, 50);
+			num1 = random(20, 30);
+			num2 = random(15, 20);
 		} else {
 			num1 = random(1600, 2400);
 			num2 = random(1000, 2000);
