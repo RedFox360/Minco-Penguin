e92221e559d34d33e415e86d10238fbb3020d01f
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-03 10:15:12 -0700

    heroku update

--- commands/returns/embed.js
+++ commands/returns/embed.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 module.exports = {
 	description: "Sends an embed to a channel",
-	usage: "!embed <Title> <Description>",
+	usage: "!embed <Title> | <Description>",
 	async execute(message, args) {
 		let td = args.join(" ").split("|");
 		let embed = new MessageEmbed().setTitle(td[0]).setDescription(td[1]);
