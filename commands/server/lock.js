036e00b99621524ce9922ae33d6f00bdbff43a10
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-05 14:55:39 -0700

    unlock command added

--- commands/server/lock.js
+++ commands/server/lock.js
@@ -3,12 +3,14 @@ const ms = require("ms");
 module.exports = {
 	description: "Locks/unlocks the current channel",
 	usage: "!lock/unlock (ms)",
+	aliases: ["unlock"],
 	/** @param {Message} message */
-	execute(message, args) {
+	execute(message, args, cmd) {
 		if (!message.member.hasPermission("MANAGE_CHANNELS") || !message.member.hasPermission("MANAGE_ROLES"))
 			return "You don't have the correct permissions to execute this command";
+		var SEND_MESSAGES = cmd === "unlock";
 		message.channel.updateOverwrite(message.guild.roles.everyone, {
-			SEND_MESSAGES: false,
+			SEND_MESSAGES,
 		});
 		let modRole = message.guild.roles.cache.find((role) => role.name === "Moderator");
 		if (modRole) {
@@ -17,7 +19,7 @@ module.exports = {
 			});
 		}
 		message.channel.send(`🔒 <#${message.channel.id}> has been locked`);
-		if (args.length) {
+		if (args.length && cmd === "lock") {
 			const time = ms(args.join(" "));
 			if (!time) return "Enter a valid time";
 			setTimeout(() => {
