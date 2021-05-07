372b962852a4be780f4e9d6afcf96646deb9a77c
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-06 19:23:16 -0700

    added states and capitalas command

---
+++ commands/hidden/sac.js
@@ -0,0 +1,90 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+	description: "Returns the states and capitals\nCredit to Aiden for the idea",
+	aliases: ["states-and-capitals", "s-and-c", "state-cap", "states-capitals"],
+	usage: "!sac (west/midwest/south/northeast)",
+	execute(message, args) {
+		const embed = new MessageEmbed().setTitle(":flag_us: States & Capitals of the US");
+
+		const west = `Olympia, Washington
+Salem, Oregon
+Sacramento, California
+Juneau, Alaska
+Honolulu, Hawaii
+Boise, Idaho
+Helene Montana
+Cheyenne, Wyoming
+Carson City, Nevada
+Salt Lake City, Utah
+Denver, Colorado
+Phoenix, Arizona,
+Santa Fe, New Mexico`;
+		const midwest = `Bismarck, North Dakota
+Pierre, South Dakota
+Lincoln, Nebraska
+Topeka, Kansas
+St. Paul, Minnesota
+Des Moines, Iowa
+Jefferson City, Missouri
+Springfield, Illinois
+Madison, Wisconsin
+Lansing, Michigan
+Indianapolis, Indiana
+Coolumbus, Ohio`;
+		const south = `Austin, Texas
+Oklahoma City, Oklahoma
+Little Rock, Arkansas,
+Baton Rouge, Louisiana
+Jackson, Mississippi
+Montgomery, Alabama,
+Tallahassee, Florida
+Atlanta, Georgia
+Nashville, Tennessee
+Frankfort, Kentucky
+Richmond, Virginia
+Raleigh, North Carolina
+Columbia, South Carolina,
+Charleston, West Virginia
+Dover, Delaware
+Annapolis, Maryland`;
+		const northeast = `Augusta, Maine
+Concord, New Hampshire
+Montpelier, Vermont
+Boston, Massachusetts
+Hartford, Connecticut
+Providence, Rhode Island
+Albany, New York
+Trenton, New Jersey
+Harrisburg, Pennsylvania`;
+
+		if (args[0] == "west") {
+			embed.setDescription(west);
+		} else if (args[0] == "midwest") {
+			embed.setDescription(midwest);
+		} else if (args[0] == "south") {
+			embed.setDescription(south);
+		} else if (args[0] == "northeast") {
+			embed.setDescription(northeast);
+		} else {
+			embed.addFields(
+				{
+					name: "Western",
+					description: west,
+				},
+				{
+					name: "Midwestern",
+					description: midwest,
+				},
+				{
+					name: "Southern",
+					description: south,
+				},
+				{
+					name: "Northeastern",
+					description: northeast,
+				}
+			);
+		}
+		message.channel.send(embed);
+	},
+};
