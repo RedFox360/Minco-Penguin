e761fb62215746c93bae89ccda3e39fc9775e15a
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-12 15:09:58 -0700

    marriage mention removed

--- commands/items/marriage.js
+++ commands/items/marriage.js
@@ -3,29 +3,12 @@ const profileModel = require("../../models/profileSchema");
 module.exports = {
 	description: "View your marriage status",
 	execute(message, _0, _1, _2, profileData) {
-		const mention = message.mentions.users.first();
-		let are = "You are";
-		let spouse, nickname, avatar, username;
-		if (mention)
-			(async () => {
-				are = `${mention.id} is`;
-				spouse = (await profileModel.findOne({ userID: mention.id })).spouse;
-				nickname = message.guild.members.cache.get(mention.id).nickname;
-				username = mention.username;
-				avatar = mention.avatarURL();
-			})();
-		else {
-			spouse = profileData.spouse;
-			nickname = message.member.nickname;
-			username = message.author.username;
-			avatar = message.author.avatarURL();
-		}
-		if (spouse == null) return `${are} not married`;
+		if (profileData.spouse == null) return `${are} not married`;
 		message.channel.send(
 			new MessageEmbed()
 				.setTitle(":ring: Marriage")
-				.setAuthor(nickname || username, avatar)
-				.setDescription(`${are} currently married to <@${spouse}>`)
+				.setAuthor(message.member.nickname || message.author.username, message.author.avatarURL())
+				.setDescription(`You are currently married to <@${profileData.spouse}>`)
 				.setColor("BEDFFF") // light blue
 		);
 	},
