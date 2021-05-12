8628078653642a154e5af60e75309885126fd02f
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-12 15:04:27 -0700

    another marriage fix ... hopefully

--- commands/items/marriage.js
+++ commands/items/marriage.js
@@ -5,27 +5,25 @@ module.exports = {
 	execute(message, _0, _1, _2, profileData) {
 		const mention = message.mentions.users.first();
 		let are = "You are";
-		let profile, nickname, avatar, username;
+		let spouse, nickname, avatar, username;
 		if (mention) {
 			are = `${mention.id} is`;
-			profileModel.findOne({ userID: mention.id }).then((a) => {
-				profile = a;
-			});
+			spouse = (await profileModel.findOne({ userID: mention.id })).spouse;
 			nickname = message.guild.members.cache.get(mention.id).nickname;
 			username = mention.username;
 			avatar = mention.avatarURL();
 		} else {
-			profile = profileData;
+			spouse = profileData.spouse;
 			nickname = message.member.nickname;
 			username = message.author.username;
 			avatar = message.author.avatarURL();
 		}
-		if (profile.spouse == null) return `${are} not married`;
+		if (spouse == null) return `${are} not married`;
 		message.channel.send(
 			new MessageEmbed()
 				.setTitle(":ring: Marriage")
 				.setAuthor(nickname || username, avatar)
-				.setDescription(`${are} currently married to <@${profile.spouse}>`)
+				.setDescription(`${are} currently married to <@${spouse}>`)
 				.setColor("BEDFFF") // light blue
 		);
 	},
