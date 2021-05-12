871d569537977b030d3b013cce23befadd42c1fc
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-11 20:14:58 -0700

    marry fix

--- commands/economy/marry.js
+++ commands/economy/marry.js
@@ -6,13 +6,13 @@ module.exports = {
 	usage: "!marry (@user)",
 	/** @param {Message} message */
 	async execute(message, _0, _1, _2, profileData) {
-		if (!profile.inventory.includes("01")) return message.channel.send("You need to buy a ring first!");
+		if (!profileData.inventory.includes("01")) return message.channel.send("You need to buy a ring first!");
 		const user = message.mentions.users.first();
 		if (!user) return message.channel.send("Mention a valid user!");
 		if (user.id == message.author.id) return message.channel.send("You can't marry yourself, obviously");
 		if (user.bot) return message.channel.send("You can't marry a bot!");
 
-		if (profile.spouse != null) return message.channel.send("You are already married!");
+		if (profileData.spouse != null) return message.channel.send("You are already married!");
 
 		const marryMsg = await message.channel.send(
 			`You have proposed to marry <@${user.id}>! <@${user.id}>, accept by reacting with a check mark.`
