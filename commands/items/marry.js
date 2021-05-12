92d8b172c68c42bcbe41f6ca5bab3c45b1a4844f
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-12 15:00:43 -0700

    more fixes

--- commands/items/marry.js
+++ commands/items/marry.js
@@ -7,15 +7,19 @@ module.exports = {
 	usage: "!marry (@user)",
 	/** @param {Message} message */
 	async execute(message, _0, _1, _2, profileData) {
-		if (!profileData.inventory.includes("01")) return message.channel.send("You need to buy a ring first!");
+		if (profileData.spouse != null) return message.channel.send("You are already married!");
+
 		const user = message.mentions.users.first();
 		if (!user) return message.channel.send("Mention a valid user!");
-		if (user.id == message.author.id) return message.channel.send("You can't marry yourself, obviously");
-		if (user.bot) return message.channel.send("You can't marry a bot!");
 
 		const userProfile = await profileModel.findOne({ userID: user.id });
 		if (userProfile.spouse != null) return message.channel.send(`<@${user.id}> is already married!`);
-		if (profileData.spouse != null) return message.channel.send("You are already married!");
+
+		if (user.id == message.author.id) return message.channel.send("You can't marry yourself, obviously");
+
+		if (user.bot) return message.channel.send("You can't marry a bot!");
+
+		if (!profileData.inventory.includes("01")) return message.channel.send("You need to buy a ring first!");
 
 		const marryMsg = await message.channel.send(
 			`You have proposed to marry <@${user.id}>! <@${user.id}>, accept by reacting with a check mark.`
