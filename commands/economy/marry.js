2c84c78de3ea64f571e155a20b03de7872b71b25
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-12 07:37:40 -0700

    hopeful fix

--- commands/economy/marry.js
+++ commands/economy/marry.js
@@ -11,7 +11,7 @@ module.exports = {
 		if (!user) return message.channel.send("Mention a valid user!");
 		if (user.id == message.author.id) return message.channel.send("You can't marry yourself, obviously");
 		if (user.bot) return message.channel.send("You can't marry a bot!");
-		const inv = profileModel.inventory.filter((a) => a !== "01");
+		const inv = profileModel?.inventory.filter((a) => a != "01");
 		await profileModel.findOneAndUpdate(
 			{ userID: message.author.id },
 			{
