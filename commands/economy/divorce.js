a46471aeacac50c650ff6dbd3a1c3ed537528a5c
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-12 07:33:51 -0700

    added @ admindivorce

--- commands/economy/divorce.js
+++ commands/economy/divorce.js
@@ -1,10 +1,23 @@
 const profileModel = require("../../models/profileSchema");
 module.exports = {
-	aliases: ["admindivorce"],
+	aliases: ["admindivorce", "aidendivorce"],
 	description:
 		"Divorce your spouse.\nThis requires a fee of 150 Minco Dollars. Your Minco Dollar money totals will be added and split between you.",
 	async execute(message, _0, cmd, _1, profileData) {
 		if (message.author.id == "724786310711214118" && cmd === "admindivorce") {
+			const mention = message.mentions.users.first();
+			if (mention) {
+				await profileModel.findOneAndUpdate(
+					{
+						userID: mention.id,
+					},
+					{
+						spouse: null,
+					}
+				);
+				message.channel.send(`You have succesfully removed the spouse of <@${mention.id}>`);
+				return;
+			}
 			await profileModel.findOneAndUpdate(
 				{
 					userID: profileData.spouse,
