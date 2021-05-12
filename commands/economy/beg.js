8715234e2673b60f503bb84396e54314bebb62f2
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-11 20:31:07 -0700

    beg marriage

--- commands/economy/beg.js
+++ commands/economy/beg.js
@@ -3,9 +3,17 @@ module.exports = {
 	cooldown: 240, //4 minutes
 	aliases: ["gimmemoney"],
 	description: "Beg for Minco Dollars",
-	async execute(message, _, cmd) {
+	async execute(message, _0, cmd, _1, profileData) {
 		let money = cmd === "gimmemoney" ? 2 : 4;
 		let numberEcon = Math.floor(Math.random() * money) + 1;
+		if (profileData.spouse != null) {
+			random = Math.floor(Math.random() * 100);
+			if (random <= 7) {
+				// 7% chance
+				numberEcon *= 2;
+				message.channel.send("You got a double bonus for marriage!");
+			}
+		}
 		await profileModel.findOneAndUpdate(
 			{ userID: message.author.id },
 			{
