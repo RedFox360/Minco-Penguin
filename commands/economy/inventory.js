795211ee82762bd348ac2b4d5e0ac986a6f0376c
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-11 20:19:15 -0700

    inventory cmd

---
+++ commands/economy/inventory.js
@@ -0,0 +1,14 @@
+module.exports = {
+	description: "View your item inventory! (from shop)",
+	execute(message, _0, _1, _2, profileData) {
+		if (!profileData.inventory.length) return "You don't have any items in your inventory.";
+		const inventory = profileData.inventory.map((t) => {
+			if (t == "01") return ":ring: Marriage Ring";
+			if (t == "02") return ":diamond_shape_with_a_dot_inside: Diamond Crown";
+			if (t == "03") return ":cowboy: Cowboy Hat";
+			if (t == "04") return ":tomato: Tomato";
+		});
+
+		inventory.forEach((i) => message.channel.send(i));
+	},
+};
