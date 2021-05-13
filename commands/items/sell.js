615834d22e800c8ada6c4e8dc6b7532750d8582f
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-13 13:45:39 -0700

    sell to user

--- commands/items/sell.js
+++ commands/items/sell.js
@@ -3,29 +3,57 @@ const removeValue = require("../../functions/removeValue");
 module.exports = {
 	description: "Sell your items!",
 	usage: "!sell <item number>",
-	execute(message, args, _0, _1, profileData) {
+	async execute(message, args, _0, _1, profileData) {
 		const itemNumber = args[0];
 		if (!itemNumber) return message.channel.send("Enter an item number");
 		if (!profileData.inventory.includes(itemNumber)) return "You don't have that item!";
 		const prices = [75, 1000, 25, 4, 12, 75, 400];
 		const inv = removeValue(itemNumber, profileData.inventory);
 		const price = prices[parseInt(itemNumber) - 1];
+		const mention = message.mentions.users.first();
+		const price = args[2];
+		if (mention && price) {
+			message.channel.send(
+				`<@${
+					mention.id
+				}>, ${message.author.toString()} has offered to sell their item ${itemNumber} for ${price} MD.\nAccept by reacting with a check mark.`
+			);
+			msg.react("✅");
+			const filter = (reaction, user) => reaction.emoji.name === "✅" && user.id === mention.id;
+			const reactionCollector = msg.createReactionCollector(filter, { time: ms("3m") });
+			reactionCollector.on("collect", () => {
+				sell(message, price, inv, itemNumber, client, true);
 
+				await profileModel.findOneAndUpdate(
+					{
+						userID: mention.id,
+					},
+					{
+						$push: {
+							inventory: itemNumber,
+						},
+						mincoDollars: -price,
+					}
+				);
+				message.channel.send(`${message.author.toString()}, you succesfully sold your item!`);
+				reactionCollector.stop();
+			});
+		}
 		if (price >= 75) {
 			const msg = message.channel.send("React to sell your item");
 			msg.react("✅");
 			const filter = (reaction, user) => reaction.emoji.name === "✅" && user.id === message.author.id;
 			const reactionCollector = msg.createReactionCollector(filter, { time: ms("30s") });
 			reactionCollector.on("collect", () => {
-				sell(message, price, inv, itemNumber, client);
+				sell(message, price, inv, itemNumber, client, false);
 			});
 		} else {
-			sell(message, price, inv, itemNumber, client);
+			sell(message, price, inv, itemNumber, client, false);
 		}
 	},
 };
-async function sell(message, price, inv, itemN, client) {
-	const amount = Math.round(price / 2 / 5) * 5;
+async function sell(message, price, inv, itemN, client, actualPrice) {
+	const amount = actualPrice ? price : Math.round(price / 2 / 5) * 5;
 	await profileModel.findOneAndUpdate(
 		{ userID: message.author.id },
 		{
