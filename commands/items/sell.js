b660704644fe2336bbb558d4d338331f0880ecb4
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-13 13:59:56 -0700

    sell remove to user

--- commands/items/sell.js
+++ commands/items/sell.js
@@ -10,35 +10,35 @@ module.exports = {
 		const prices = [75, 1000, 25, 4, 12, 75, 400];
 		const inv = removeValue(itemNumber, profileData.inventory);
 		const price = prices[parseInt(itemNumber) - 1];
-		const mention = message.mentions.users.first();
-		const price = args[2];
-		if (mention && price) {
-			message.channel.send(
-				`<@${
-					mention.id
-				}>, ${message.author.toString()} has offered to sell their item ${itemNumber} for ${price} MD.\nAccept by reacting with a check mark.`
-			);
-			msg.react("✅");
-			const filter = (reaction, user) => reaction.emoji.name === "✅" && user.id === mention.id;
-			const reactionCollector = msg.createReactionCollector(filter, { time: ms("3m") });
-			reactionCollector.on("collect", () => {
-				sell(message, price, inv, itemNumber, client, true);
+		// const mention = message.mentions.users.first();
+		// const price = args[2];
+		// if (mention && price) {
+		// 	message.channel.send(
+		// 		`<@${
+		// 			mention.id
+		// 		}>, ${message.author.toString()} has offered to sell their item ${itemNumber} for ${price} MD.\nAccept by reacting with a check mark.`
+		// 	);
+		// 	msg.react("✅");
+		// 	const filter = (reaction, user) => reaction.emoji.name === "✅" && user.id === mention.id;
+		// 	const reactionCollector = msg.createReactionCollector(filter, { time: ms("3m") });
+		// 	reactionCollector.on("collect", () => {
+		// 		sell(message, price, inv, itemNumber, client, true);
 
-				await profileModel.findOneAndUpdate(
-					{
-						userID: mention.id,
-					},
-					{
-						$push: {
-							inventory: itemNumber,
-						},
-						mincoDollars: -price,
-					}
-				);
-				message.channel.send(`${message.author.toString()}, you succesfully sold your item!`);
-				reactionCollector.stop();
-			});
-		}
+		// 		await profileModel.findOneAndUpdate(
+		// 			{
+		// 				userID: mention.id,
+		// 			},
+		// 			{
+		// 				$push: {
+		// 					inventory: itemNumber,
+		// 				},
+		// 				mincoDollars: -price,
+		// 			}
+		// 		);
+		// 		message.channel.send(`${message.author.toString()}, you succesfully sold your item!`);
+		// 		reactionCollector.stop();
+		// 	});
+		// }
 		if (price >= 75) {
 			const msg = message.channel.send("React to sell your item");
 			msg.react("✅");
