7e5916d9c1c76d638c45495383e69111679b1789
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-13 12:41:35 -0700

    animal cooldown

--- commands/items/animal.js
+++ commands/items/animal.js
@@ -1,8 +1,9 @@
 const animals = require("../../functions/animals.json");
 const profileModel = require("../../models/profileSchema");
+const ms = require("ms");
 module.exports = {
 	description: "Pay 20 MD for a random animal!",
-	cooldown: 1200, // 20 minutes
+	cooldown: ms("15m") / 1000, // 15 minutes
 	async execute(message, _0, _1, _2, profileData) {
 		if (profileData.zoo.length >= 15) return message.channel.send("You have reached the maximum amount of animals (15)");
 		if (profileData.mincoDollars < 20) return message.channel.send("You need 20 Minco Dollars to buy an animal");
