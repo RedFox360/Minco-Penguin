40d7af0329929378f024a09f7c689e87cc07383b
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-12 18:38:16 -0700

    sell removes crown role

--- commands/items/sell.js
+++ commands/items/sell.js
@@ -17,15 +17,14 @@ module.exports = {
 			const filter = (reaction, user) => reaction.emoji.name === "✅" && user.id === message.author.id;
 			const reactionCollector = msg.createReactionCollector(filter, { time: ms("30s") });
 			reactionCollector.on("collect", () => {
-				sell(message, price, inv);
+				sell(message, price, inv, itemNumber, client);
 			});
 		} else {
-			sell(message, price, inv);
+			sell(message, price, inv, itemNumber, client);
 		}
 	},
 };
-
-async function sell(message, price, inv) {
+async function sell(message, price, inv, itemN, client) {
 	const amount = Math.round(price / 2 / 5) * 5;
 	await profileModel.findOneAndUpdate(
 		{ userID: message.author.id },
@@ -36,6 +35,11 @@ async function sell(message, price, inv) {
 			inventory: inv,
 		}
 	);
-
+	if (itemN == "02") {
+		const carrelCrew = client.guilds.cache.get("785642761814671381");
+		const role = carrelCrew.roles.cache.get("842053621402173501");
+		const member = carrelCrew.members.cache.get(message.author.id);
+		member.roles.remove(role);
+	}
 	message.channel.send(`You sold your item for ${amount} MD`);
 }
