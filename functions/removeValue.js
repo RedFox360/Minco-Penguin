f7441dcd331f740b9c8aef0873dd8b5a6b2dfc93
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-13 15:30:04 -0700

    removeValue fix...

--- functions/removeValue.js
+++ functions/removeValue.js
@@ -1,8 +1,7 @@
 function removeValue(value, array) {
-	for (var i = 0; i < array.length; i++) {
+	for (var i = array.length - 1; i >= 0; i++) {
 		if (array[i] === value) {
 			array.splice(i, 1);
-			i--;
 		}
 	}
 	return array;
