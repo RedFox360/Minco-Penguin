f0d712b3a36c1ce9107f91286711727b37778765
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-13 18:27:55 -0700

    animalFunctions nicer

--- functions/animalFunctions.js
+++ functions/animalFunctions.js
@@ -1,26 +1,21 @@
 const animals = require("./animals.json");
-function animalExists(animal) {
-	for (let { name } of animals) {
-		if (name.toLowerCase() == animal.toLowerCase()) return true;
-	}
-	return false;
-}
-function getAnimal(animal) {
-	for (let a of animals) {
-		if (animal.toLowerCase() == a.name.toLowerCase()) return a;
-	}
-	return null;
-}
-
-function hasAnimal(animal, profileData) {
-	for (let { name } of profileData.zoo) {
-		if (animal.toLowerCase() == name.toLowerCase()) return true;
-	}
-	return false;
-}
-
 module.exports = {
-	animalExists,
-	getAnimal,
-	hasAnimal,
+	animalExists(animal) {
+		for (let { name } of animals) {
+			if (name.toLowerCase() == animal.toLowerCase()) return true;
+		}
+		return false;
+	},
+	getAnimal(animal) {
+		for (let a of animals) {
+			if (animal.toLowerCase() == a.name.toLowerCase()) return a;
+		}
+		return null;
+	},
+	hasAnimal(animal, profileData) {
+		for (let { name } of profileData.zoo) {
+			if (animal.toLowerCase() == name.toLowerCase()) return true;
+		}
+		return false;
+	},
 };
