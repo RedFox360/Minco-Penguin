41c9bad166ecb23ea9baf64f719fbe17395296f5
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-15 10:47:35 -0700

    shop gems

--- commands/items/shop.js
+++ commands/items/shop.js
@@ -1,64 +1,99 @@
 const { MessageEmbed } = require("discord.js");
+const ms = require("ms");
 module.exports = {
 	description: "The Minco shop! Use it to view the items you can buy",
-	execute(message) {
+	async execute(message) {
+		const descriptions = ["Buy these using !buy <item number>", "Buy these using !buy-gem <item number>"];
+		const titles = ["Minco Shop | Basics", "Minco Shop | Gems"];
+		let currentPage = 0;
+		const fields = [
+			[
+				[":ring: (01) | Marriage Ring", "75 Minco Dollars\nCan be used to propose to someone"],
+				[":diamond_shape_with_a_dot_inside: (02) | Diamond Crown", "1000 Minco Dollars"],
+				[":cowboy: (03) | Cowboy Hat", "25 Minco Dollars"],
+				[
+					":tomato: (04) | Tomato",
+					"4 Minco Dollars\nA fresh ripe tomato, can be eaten for between 2 and 6 Minco Dollars (use !tomato)",
+				],
+				[":candy: (05) | Candy", "12 Minco Dollars\nDoubles beg reward for the next 3 begs"],
+				[
+					"<:transparent_jellybot:833491227995013130> (06) | Jellyfish",
+					"75 Minco Dollars\nAllows you to sting people using the !sting command. Gives a +12 attack bonus in battle.",
+				],
+				[
+					":bear: (07) | Pet Bear",
+
+					"400 Minco Dollars\nIf you don't want to marry someone: (lower reward)\n5% chance of doubling beg reward, 33 Minco Dollar math reward",
+				],
+				[":cactus: (08) | Cactus", "50 Minco Dollars | Gives a +8 defense bonus in battle."],
+				[":fire: (09) | Fire", "50 Minco Dollars | Gives +6 attack -3 health in battle."],
+				[
+					"<:cardboard_box:843173235549667349> (10) | Lootbox",
+					"60 Minco Dollars | Gives a random reward worth between 50 and 100 MD",
+				],
+				[
+					":art: Drawing",
+
+					"!buy drawing <drawing>\nUse this to send a request to Claire for a personalized drawing! (price will be decided by her)",
+				],
+				[
+					"<:doge:797152138467082250> Meme",
+					"!buy meme <meme>\nUse this to send a request to Mason L for a custom meme! (price will be decided by him)",
+				],
+			],
+			[
+				["(01) <:blue_diamond:843178044894216202> | Blue Diamond", "100 MD"],
+				["(02) <:pink_diamond:843177780946010132> | Pink Diamond", "150 MD"],
+				["(03) <:emerald:843180288984219689> | Emerald", "150 MD"],
+				["(04) <:gold_bar:843180638705287188> | Gold Bar", "75 MD"],
+				["(05) <:sapphire:843182746050232340> | Sapphire", "75 MD"],
+			],
+		];
 		const shopEmbed = new MessageEmbed()
 			.setAuthor(message.member.nickname || message.author.username, message.author.avatarURL())
 			.setTitle("Minco Shop")
-			.addFields(
-				{
-					name: ":ring: (01) | Marriage Ring",
-					value: "75 Minco Dollars\nCan be used to propose to someone",
-				},
-				{
-					name: ":diamond_shape_with_a_dot_inside: (02) | Diamond Crown",
-					value: "1000 Minco Dollars",
-				},
-				{
-					name: ":cowboy: (03) | Cowboy Hat",
-					value: "25 Minco Dollars",
-				},
-				{
-					name: ":tomato: (04) | Tomato",
-					value: "4 Minco Dollars\nA fresh ripe tomato, can be eaten for between 2 and 6 Minco Dollars (use !tomato)",
-				},
-				{
-					name: ":candy: (05) | Candy",
-					value: "12 Minco Dollars\nDoubles beg reward for the next 3 begs",
-				},
-				{
-					name: "<:transparent_jellybot:833491227995013130> (06) | Jellyfish",
-					value: "75 Minco Dollars\nAllows you to sting people using the !sting command. Gives a +12 attack bonus in battle.",
-				},
-				{
-					name: ":bear: (07) | Pet Bear",
-					value:
-						"400 Minco Dollars\nIf you don't want to marry someone: (lower reward)\n5% chance of doubling beg reward, 33 Minco Dollar math reward",
-				},
-				{
-					name: ":cactus: (08) | Cactus",
-					value: "50 Minco Dollars | Gives a +8 defense bonus in battle.",
-				},
-				{
-					name: ":fire: (09) | Fire",
-					value: "50 Minco Dollars | Gives +6 attack -3 health in battle.",
-				},
-				{
-					name: "<:cardboard_box:843173235549667349> (10) | Lootbox",
-					value: "60 Minco Dollars | Gives a random reward worth between 50 and 100 MD",
-				},
-				{
-					name: ":art: Drawing",
-					value:
-						"!buy drawing <drawing>\nUse this to send a request to Claire for a personalized drawing! (price will be decided by her)",
-				},
-				{
-					name: "<:doge:797152138467082250> Meme",
-					value: "!buy meme <meme>\nUse this to send a request to Mason L for a custom meme! (price will be decided by him)",
-				}
-			)
 			.setColor("BEDFFF")
 			.setFooter(message.guild.name);
-		message.channel.send(shopEmbed);
+		for (let field of fields[0]) {
+			shopEmbed.addField(field[0], field[1]);
+		}
+		const shopMsg = await message.channel.send(shopEmbed);
+		try {
+			await shopMsg.react("⬅️");
+			await shopMsg.react("➡️");
+		} catch (err) {
+			console.error(err);
+		}
+		const filter = (_, user) => user.id === message.author.id;
+		const collector = helpMsg.createReactionCollector(filter, { time: ms("4m") });
+		collector.on("collect", async (reaction) => {
+			if (reaction.emoji.name == "⬅️") {
+				if (currentPage != 0) currentPage--;
+				if (currentPage == 0) {
+					shopEmbed.setTitle(titles[0]);
+					shopEmbed.setTitle(titles[currentPage]);
+					fields[currentPage].forEach((field) => {
+						shopEmbed.addField(field[0], field[1]);
+					});
+					helpMsg.edit(shopEmbed);
+				}
+			} else if (reaction.emoji.name == "➡️") {
+				if (currentPage != titles.length - 1) currentPage++;
+				shopEmbed.setTitle(titles[currentPage]);
+				fields[currentPage].forEach((field) => {
+					shopEmbed.addField(field[0], field[1]);
+				});
+				helpMsg.edit(shopEmbed);
+				helpMsg.edit(shopEmbed);
+			}
+			const userReactions = helpMsg.reactions.cache.filter((react) => react.users.cache.has(message.author.id));
+			try {
+				for (const reaction of userReactions.values()) {
+					await reaction.users.remove(message.author.id);
+				}
+			} catch (error) {
+				console.error("Failed to remove reactions.");
+			}
+		});
 	},
 };
