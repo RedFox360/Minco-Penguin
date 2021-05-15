ca1e0ce94ec6d2273e52fad74aea3d0bb8ef17d5
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-15 12:54:40 -0700

    buy gem usage

--- commands/items/buy-gem.js
+++ commands/items/buy-gem.js
@@ -1,5 +1,6 @@
 module.exports = {
 	description: "Buy a gem!",
+	usage: "!buy <gem number>",
 	execute(message, args, _0, _1, profileData) {
 		if (args[0] == "01") {
 			buyGem(message, "Blue Diamond <:blue_diamond:843178044894216202>", 100, "01", profileData);
