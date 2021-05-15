3750ceb487fae6343167da9419dc6b66f07ff721
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-15 08:26:38 -0700

    animal slowmode less

--- commands/items/animal.js
+++ commands/items/animal.js
@@ -4,7 +4,7 @@ const ms = require("ms");
 const { hasAnimal } = require("../../functions/animalFunctions");
 module.exports = {
 	description: "Pay 20 MD for a random animal!",
-	cooldown: ms("15m") / 1000, // 15 minutes
+	cooldown: ms("12m") / 1000, // 15 minutes
 	async execute(message, _0, _1, _2, profileData) {
 		if (profileData.zoo.length >= 15) return "You have reached the maximum amount of animals (15)";
 		if (profileData.mincoDollars < 20) return "You need 20 Minco Dollars to buy an animal";
