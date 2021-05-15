5b055de84a20379d54ed2e85c6ca00de096136c1
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-15 11:37:25 -0700

    random JSDoc

--- functions/random.js
+++ functions/random.js
@@ -1,3 +1,8 @@
+/**
+ * Simple random command
+ * @param {number} min The minimum number, included
+ * @param {number} max The maximum number, included
+ */
 function randomIntFromInterval(min, max) {
 	// min and max included
 	return Math.floor(Math.random() * (max - min + 1) + min);
