a2db28148878d535b398fe49f6dd2bf7adb1839f
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-16 15:25:11 -0700

    reset fix

--- commands/economy/reset.js
+++ commands/economy/reset.js
@@ -5,23 +5,14 @@ module.exports = {
 	async execute(message) {
 		if (message.author.id == "724786310711214118") {
 			const mention = message.mentions.users.first();
-			const profile = await profileModel.findOne({ userID: mention.id });
 			await profileModel.findOneAndUpdate(
 				{ userID: mention.id },
 				{
-					userID: mention.id,
-					serverID: message.guild.id,
 					mincoDollars: 100,
 					bank: 0,
-					birthday: profile.birthday,
-					favs: {
-						food: profile.favs.food,
-						color: profile.favs.color,
-						animal: profile.favs.animal,
-					},
 				}
 			);
-			message.channel.send(`Profile for <@${mention.id}> has been reset.`);
+			message.channel.send(`Minco Dollars for <@${mention.id}> has been reset.`);
 		}
 	},
 };
