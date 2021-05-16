3c65ff41970561e27404eaed6f40635aad529e21
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-16 13:49:57 -0700

    balance shows author

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -9,17 +9,21 @@ module.exports = {
 	/** @param {Message} message */
 	async execute(message, _0, _1, _2, profileData) {
 		const mention = message.mentions.users.first();
-		var md, bank;
+
+		let md = profileData.mincoDollars;
+		let bank = profileData.bank;
+		let author = message.author;
+		let nickname = message.member.nickname;
 		if (mention) {
 			let profile = await profileModel.findOne({ userID: mention.id });
 			md = profile.mincoDollars;
 			bank = profile.bank;
-		} else {
-			md = profileData.mincoDollars;
-			bank = profileData.bank;
+			author = mention;
+			nickname = message.guild.members.cache.get(mention.id);
 		}
 
 		let balanceEmbed = new MessageEmbed()
+			.setAuthor(nickname || author.username, author.avatarURL())
 			.setTitle("Balance")
 			.setColor("7BFF70")
 			.setDescription(
