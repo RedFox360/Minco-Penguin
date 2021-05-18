f406f7a57ebbc2247d121b4d25f762433423e792
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-18 10:32:22 -0700

    green diamond command

---
+++ commands/hidden/green-diamond.js
@@ -0,0 +1,23 @@
+module.exports = {
+	description: "[sameer only] Gives the green diamond to a user",
+	usage: "!green-diamond <@user>",
+	async execute(message) {
+		if (message.author.id == "724786310711214118") {
+			const mention = message.mentions.users.first();
+			if (!mention) return "Mention a valid user";
+
+			await profileModel.findOneAndUpdate(
+				{
+					userID: mention.id,
+				},
+				{
+					$push: {
+						gems: "157",
+					},
+				}
+			);
+
+			message.channel.send(`You gave the exclusive GREEN DIAMOND to <@${mention.id}>`);
+		}
+	},
+};
