ac31be6bad457706241d69e93b72200cdb5b5c0c
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-17 18:17:04 -0700

    market buy fix

--- commands/items/market-buy.js
+++ commands/items/market-buy.js
@@ -4,13 +4,13 @@ module.exports = {
 	description: "Buy items from a user's market!",
 	aliases: ["mb"],
 	usage: "!market-buy <@user> <item>",
-	async execute(message, args) {
+	async execute(message, args, _0, _1, profileData) {
 		if (args.length < 1) return "Valid usage: !market-add <price> <item name>";
 		const mention = message.mentions.users.first();
 		if (!mention) return "Mention a valid user";
 		args.shift();
 		const item = args.join(" ");
-		const profile = await profileModel.findOne({ userID: message.author.id });
+		const profile = await profileModel.findOne({ userID: mention.id });
 		if (!hasItem(item, profile)) return `<@${mention.id}> does not have that item! (remember capitalization)`;
 		const i = getItem(item, profile);
 		if (profileData.mincoDollars < i.price) {
@@ -29,11 +29,6 @@ module.exports = {
 					$inc: {
 						mincoDollars: i.price,
 					},
-					$pull: {
-						market: {
-							name: item,
-						},
-					},
 				}
 			);
 			await profileModel.findOneAndUpdate(
@@ -44,18 +39,19 @@ module.exports = {
 					$inc: {
 						mincoDollars: -i.price,
 					},
-					$push: {
-						market: i,
-					},
 				}
 			);
-			message.channel.send(`You bought **${i.name}** for **${i.price}** MD!`);
+			mention.send(`${message.author.id} has bought your **${i.name}**!`);
+			message.channel.send(
+				`You bought **${i.name}** for **${i.price}** MD! <@${mention.id}> will be DMed notifying your purchase.`
+			);
 		});
 	},
 };
 function hasItem(item, profileData) {
-	for (const { name } of profileData.market) {
-		if (item == name) return true;
+	for (const m of profileData.market) {
+		console.log(m);
+		if (item == m.name) return true;
 	}
 	return false;
 }
