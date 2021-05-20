058774cadf30f4588239010832c1844db70d9b84
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-20 10:33:11 -0700

    added author+nickname

--- commands/items/marriage.js
+++ commands/items/marriage.js
@@ -6,16 +6,20 @@ module.exports = {
 	async execute(message, _0, _1, _2, profileData) {
 		let profile = profileData;
 		let are = "You are";
+		let author = message.author;
+		let nickname = message.member.nickname;
 		const mention = message.mentions.users.first();
 		if (mention) {
 			profile = await profileModel.findOne({ userID: mention.id });
 			are = `<@${mention.id}> is`;
+			author = mention;
+			nickname = message.guild.members.cache.get(mention.id).nickname;
 		}
 		if (profile.spouse == null) return `${are} not married`;
 		message.channel.send(
 			new MessageEmbed()
 				.setTitle(":ring: Marriage")
-				.setAuthor(message.member.nickname, message.author.avatarURL())
+				.setAuthor(nickname || author.username, author.avatarURL())
 				.setDescription(`${are} currently married to <@${profile.spouse}>`)
 				.setColor("BEDFFF") // light blue
 		);
