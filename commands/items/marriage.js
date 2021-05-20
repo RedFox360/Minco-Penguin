163d3b211a29d7abf876a43d38d5e714e89fde20
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-20 08:11:09 -0700

    !marriage @user

--- commands/items/marriage.js
+++ commands/items/marriage.js
@@ -1,13 +1,21 @@
 const { MessageEmbed } = require("discord.js");
+const profileModel = require("../../models/profileSchema");
 module.exports = {
 	description: "View your marriage status",
-	execute(message, _0, _1, _2, profileData) {
-		if (profileData.spouse == null) return `You are not married`;
+	async execute(message, _0, _1, _2, profileData) {
+		const profile = profileData;
+		const are = "You are";
+		const mention = message.mentions.users.first();
+		if (mention) {
+			profile = await profileModel.findOne({ userID: mention.id });
+			are = `<@${mention.id}> is`;
+		}
+		if (profile.spouse == null) return `${are} not married`;
 		message.channel.send(
 			new MessageEmbed()
 				.setTitle(":ring: Marriage")
 				.setAuthor(message.member.nickname, message.author.avatarURL())
-				.setDescription(`You are currently married to <@${profileData.spouse}>`)
+				.setDescription(`${are} currently married to <@${profile.spouse}>`)
 				.setColor("BEDFFF") // light blue
 		);
 	},
