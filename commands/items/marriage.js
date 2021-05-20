3e57d6a26df10743cc94c3c0e148e623bf06385c
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-20 08:13:09 -0700

    const -> let

--- commands/items/marriage.js
+++ commands/items/marriage.js
@@ -4,8 +4,8 @@ module.exports = {
 	description: "View your marriage status",
 	usage: "!marriage (@user)",
 	async execute(message, _0, _1, _2, profileData) {
-		const profile = profileData;
-		const are = "You are";
+		let profile = profileData;
+		let are = "You are";
 		const mention = message.mentions.users.first();
 		if (mention) {
 			profile = await profileModel.findOne({ userID: mention.id });
