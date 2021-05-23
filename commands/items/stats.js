b7cf0104f6b6ffc3f28102c98268f8d5c53353a1
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-23 13:50:16 -0700

    stats fix

--- commands/items/stats.js
+++ commands/items/stats.js
@@ -5,30 +5,35 @@ module.exports = {
 	async execute(message) {
 		const mention = message.mentions.users.first();
 		let name = message.member.displayName;
-		let avatarURL = message.author.avatarURL();
+		let author = message.author;
 		if (mention) {
 			name = message.guild.members.cache.get(mention.id).displayName;
-			avatarURL = mention.avatarURL();
+			author = mention;
 		}
 		const [attack, defense, health] = await calculatePower(author.id);
 		message.channel.send(
-			new MessageEmbed().setAuthor(name, avatarURL).setTitle("Stats").setColor("#F5B041").setFooter(message.guild.name).addFields(
-				{
-					name: ":fire:Attack",
-					value: attack,
-					inline: true,
-				},
-				{
-					name: ":crossed_swords: Defense",
-					value: defense,
-					inline: true,
-				},
-				{
-					name: ":heart: Health",
-					value: health,
-					inline: true,
-				}
-			)
+			new MessageEmbed()
+				.setAuthor(name, author.avatarURL())
+				.setTitle("Stats")
+				.setColor("#F5B041")
+				.setFooter(message.guild.name)
+				.addFields(
+					{
+						name: ":fire:Attack",
+						value: attack,
+						inline: true,
+					},
+					{
+						name: ":crossed_swords: Defense",
+						value: defense,
+						inline: true,
+					},
+					{
+						name: ":heart: Health",
+						value: health,
+						inline: true,
+					}
+				)
 		);
 	},
 };
