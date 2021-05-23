321878a203cf499323b8a0d85bdd6feace6abf89
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-23 10:48:46 -0700

    sell-orbs is async

--- commands/items/sell-orbs.js
+++ commands/items/sell-orbs.js
@@ -1,7 +1,7 @@
 module.exports = {
 	description: "Sell your orbs for money (1 orb = 10 MD)",
 	usage: "!sell-orbs <number of orbs>",
-	execute(message, args, _0, _1, profileData) {
+	async execute(message, args, _0, _1, profileData) {
 		const amount = parseInt(args[0]);
 		if (isNaN(amount)) return "Enter a valid number";
 		const price = amount * 10;
