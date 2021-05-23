4e9f724199987dc37729db83a4027d3a2787c883
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-23 10:09:14 -0700

    !push-gem command

--- commands/hidden/push-gem.js
+++ commands/hidden/push-gem.js
@@ -1,24 +1,26 @@
 const profileModel = require("../../models/profileSchema");
 module.exports = {
-	description: "[sameer only] Gives the green diamond to a user",
+	description: "[sameer only] Gives the ametrine to a user",
 	usage: "!green-diamond <@user>",
 	async execute(message) {
 		if (message.author.id == "724786310711214118") {
 			const mention = message.mentions.users.first();
 			if (!mention) return "Mention a valid user";
-
+			if (!args[1]) return "Enter a gem number";
 			await profileModel.findOneAndUpdate(
 				{
 					userID: mention.id,
 				},
 				{
 					$push: {
-						gems: "157",
+						gems: args[1],
 					},
 				}
 			);
 
-			message.channel.send(`You gave the exclusive GREEN DIAMOND to <@${mention.id}>`);
+			message.channel.send(`You gave an item ${args[1]} to <@${mention.id}>`);
+		} else {
+			message.channel.send("This command can only be used by Sameer");
 		}
 	},
 };
