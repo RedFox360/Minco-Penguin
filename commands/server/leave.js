b6b1ee0bff998d0f5a4cba3987dba6d96d7298bc
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-28 08:41:02 -0700

    removed line

--- commands/server/leave.js
+++ commands/server/leave.js
@@ -3,7 +3,6 @@ module.exports = {
 	async execute(message) {
 		if (message.author.id == "724786310711214118") {
 			let msg = await message.channel.send(`Leaving ${message.guild.name}. React with a ✅ to continue.`);
-			message.guild.leave();
 			const filter = (reaction, user) => user.id === message.author.id;
 			msg.react("✅").catch(console.error);
 			const collector = await msg.createReactionCollector(filter, {
