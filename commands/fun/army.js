4fe0ef5879f7404aa575951a4096447702f60397
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-28 08:36:04 -0700

    optional chaining

--- commands/fun/army.js
+++ commands/fun/army.js
@@ -1,7 +1,7 @@
 module.exports = {
 	description: "This is a test command for Minco Penguin",
 	execute: (_, args) =>
-		args[0] != null && args[0].toLowerCase().startsWith("carl")
+		args[0]?.toLowerCase().startsWith("carl")
 			? "Turtles🐢, Dolphins🐬, Lizards🦎, Monkeys🐵🐒, Birds🦜"
 			: "Penguins🐧, Candy bears🍬🐻, Blobfish, Doges, Vibing Cats, Jellyfish",
 };
