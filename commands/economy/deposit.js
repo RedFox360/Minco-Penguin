b647d5f5edf6c4775f899cec1c71f164d9067560
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-30 15:49:52 -0700

    removed accounts

--- commands/economy/deposit.js
+++ commands/economy/deposit.js
@@ -7,7 +7,7 @@ module.exports = {
 	async execute(message, args, _0, _1, profileData) {
 		const amount = parseInt(args[0]);
 		if (isNaN(amount)) return "Enter a valid number";
-		if (amount % 1 != 0 || amount <= 0) return "Deposit amount must be a whole number";
+		if (amount <= 0) return "Deposit amount must be a positive number";
 		try {
 			if (amount > profileData.mincoDollars) return "You don't have that amount of Minco Dollars to deposit.";
 			await profileModel.findOneAndUpdate(
