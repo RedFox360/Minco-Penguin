4925bf03b08eeaa539979b305b7d12355c6cae75
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-31 11:09:56 -0700

    hid some commands

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,4 @@
 const { Client } = require("discord.js");
-const { readFileSync } = require("fs");
 /** @param {Client} client */
 module.exports = (client) => {
 	console.log(`${client.user.tag} is online!`);
