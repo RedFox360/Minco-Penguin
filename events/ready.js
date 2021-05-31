6557805f3cf201fe6e7ac45037a468943b1a523c
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-31 10:50:39 -0700

    status

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,11 @@
 const { Client } = require("discord.js");
+const { readFileSync } = require("fs");
 /** @param {Client} client */
 module.exports = (client) => {
+	const status = readFileSync("../functions/status.txt").toString().split("\n");
 	console.log(`${client.user.tag} is online!`);
+	client.user.setActivity(status[1], { type: status[0] });
+	client.user.setActivity({
+		type: "",
+	});
 };
