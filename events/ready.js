4859731611dabc16f925135bef49f73c5e2d0190
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-05-31 10:53:43 -0700

    status fix ..

--- events/ready.js
+++ events/ready.js
@@ -2,10 +2,8 @@ const { Client } = require("discord.js");
 const { readFileSync } = require("fs");
 /** @param {Client} client */
 module.exports = (client) => {
-	const status = readFileSync("../functions/status.txt").toString().split("\n");
 	console.log(`${client.user.tag} is online!`);
-	client.user.setActivity(status[1], { type: status[0] });
-	client.user.setActivity({
-		type: "",
+	client.user.setActivity("!help for help", {
+		type: "PLAYING",
 	});
 };
