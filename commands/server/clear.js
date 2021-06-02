132acdb19d461aa58ae99a643fd8304b0bb7c698
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-06-02 14:54:41 -0700

    removed await

--- commands/server/clear.js
+++ commands/server/clear.js
@@ -1,14 +1,14 @@
 module.exports = {
 	description: "[MANAGE MESSAGES] Clears [number] messages from a text channel",
 	usage: "!clear <number>",
-	async execute(message, args) {
+	execute(message, args) {
 		if (message.author.id == "769313131108237322") return "You don't have the correct permissions to execute this command.";
 		if (message.member.hasPermission("MANAGE_MESSAGES")) {
 			if (!args[0]) return "Please enter the amount of messages that you want to clear";
 			if (isNaN(args[0])) return "Please enter a valid number";
 			if (args[0] > 100) return "You cannot delete more than 100 messages!";
 			if (args[0] < 1) return "You must delete at least one message";
-			await message.channel.messages.fetch({ limit: args[0] }).then((messages) => {
+			message.channel.messages.fetch({ limit: args[0] }).then((messages) => {
 				message.channel.bulkDelete(messages);
 			});
 		} else {
