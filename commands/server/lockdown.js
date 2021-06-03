8c94f239bb043e19d5de1ead440ec91e71603cc4
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-06-03 12:52:35 -0700

    announcement channels excluded

--- commands/server/lockdown.js
+++ commands/server/lockdown.js
@@ -9,21 +9,25 @@ module.exports = {
 			const SEND_MESSAGES = cmd === "lockdown" ? false : true;
 			let modRole = message.guild.roles.cache.find((role) => role.name === "Moderator");
 			let muteRole = message.guild.roles.cache.find((role) => role.name === "Muted");
-			for (const channel of message.guild.channels.cache.array()) {
-				if (channel.permissionsFor(message.guild.roles.everyone).has("SEND_MESSAGES", false)) {
-					channel.updateOverwrite(message.guild.roles.everyone, {
+			message.guild.channels.cache.array().forEach((channel) => {
+				if (
+					channel.name.includes("announcements") ||
+					channel.parent.name.toLowerCase().includes("info") ||
+					channel.name.includes("broadcast")
+				)
+					return;
+				channel.updateOverwrite(message.guild.roles.everyone, {
+					SEND_MESSAGES,
+				});
+				if (modRole)
+					channel.updateOverwrite(modRole, {
 						SEND_MESSAGES,
 					});
-					if (modRole)
-						channel.updateOverwrite(modRole, {
-							SEND_MESSAGES,
-						});
-					if (muteRole)
-						channel.updateOverwrite(muteRole, {
-							SEND_MESSAGES,
-						});
-				}
-			}
+				if (muteRole)
+					channel.updateOverwrite(muteRole, {
+						SEND_MESSAGES,
+					});
+			});
 			if (cmd === "lockdown") {
 				message.channel.send(`🔒 ${message.guild.name} has been put on lockdown`);
 			} else {
