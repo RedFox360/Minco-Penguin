aba5961d3c6dd5b622af94a838b3c4fcdd2dd312
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-06-03 12:48:18 -0700

    checks for admin

--- commands/server/lockdown.js
+++ commands/server/lockdown.js
@@ -5,28 +5,32 @@ module.exports = {
 	aliases: ["end-lockdown"],
 	/** @param {Message} message */
 	execute(message, _0, cmd) {
-		const SEND_MESSAGES = cmd === "lockdown" ? false : true;
-		let modRole = message.guild.roles.cache.find((role) => role.name === "Moderator");
-		let muteRole = message.guild.roles.cache.find((role) => role.name === "Muted");
-		for (const channel of message.guild.channels.cache.array()) {
-			if (channel.permissionsFor(message.guild.roles.everyone).has("SEND_MESSAGES", false)) {
-				channel.updateOverwrite(message.guild.roles.everyone, {
-					SEND_MESSAGES,
-				});
-				if (modRole)
-					channel.updateOverwrite(modRole, {
+		if (message.member.hasPermission("ADMINISTRATOR")) {
+			const SEND_MESSAGES = cmd === "lockdown" ? false : true;
+			let modRole = message.guild.roles.cache.find((role) => role.name === "Moderator");
+			let muteRole = message.guild.roles.cache.find((role) => role.name === "Muted");
+			for (const channel of message.guild.channels.cache.array()) {
+				if (channel.permissionsFor(message.guild.roles.everyone).has("SEND_MESSAGES", false)) {
+					channel.updateOverwrite(message.guild.roles.everyone, {
 						SEND_MESSAGES,
 					});
-				if (muteRole)
-					channel.updateOverwrite(muteRole, {
-						SEND_MESSAGES,
-					});
+					if (modRole)
+						channel.updateOverwrite(modRole, {
+							SEND_MESSAGES,
+						});
+					if (muteRole)
+						channel.updateOverwrite(muteRole, {
+							SEND_MESSAGES,
+						});
+				}
 			}
-		}
-		if (cmd === "lockdown") {
-			message.channel.send(`🔒 ${message.guild.name} has been put on lockdown`);
+			if (cmd === "lockdown") {
+				message.channel.send(`🔒 ${message.guild.name} has been put on lockdown`);
+			} else {
+				message.channel.send(`✅ ${message.guild.name} has been unlocked`);
+			}
 		} else {
-			message.channel.send(`✅ ${message.guild.name} has been unlocked`);
+			return "This command can only be used by admins";
 		}
 	},
 };
