c6c3714771299393b145f2e457887ec9ac5bc54f
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-06-09 08:09:44 -0700

    mafia fix

--- commands/fun/mafia.js
+++ commands/fun/mafia.js
@@ -9,8 +9,8 @@ module.exports = {
 		const mafia2 = members.splice(Math.floor(Math.random() * members.length), 1);
 		const sheriff = members.splice(Math.floor(Math.random() * members.length), 1);
 		const doctor = members.splice(Math.floor(Math.random() * members.length), 1);
-
-		message.author.send(`Mafia: <@${mafia1[0].id} and <@${mafia2[0].id}>`);
+		message.channel.send(`Mafia roles sent to ${message.author.toString()}`);
+		message.author.send(`Mafia: <@${mafia1[0].id}> and <@${mafia2[0].id}>`);
 		message.author.send(`Doctor: <@${doctor[0].id}>`);
 		message.author.send(`Sheriff: <@${sheriff[0].id}>`);
 	},
