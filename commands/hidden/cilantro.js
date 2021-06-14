51475985f823d27450419ddb54383158480d06a9
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-06-14 13:34:46 -0700

    moving folderes

---
+++ commands/hidden/cilantro.js
@@ -0,0 +1,5 @@
+module.exports = {
+	description: "a little information on cilantro...",
+	execute: () =>
+		"you should be open to trying new food, unless you have a genetic variation in your olfactory-receptor genes that allow them to more strongly perceive the soapy-flavored aldehydes in cilantro leaves, which means you are just unlucky. most Indian foods contain cilantro",
+};
