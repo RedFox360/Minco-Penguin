2b0a4e48bb2388f72efa0d45095a2dc93a8627a3
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-06-16 13:11:14 -0700

    reason bette

--- commands/server/ban.js
+++ commands/server/ban.js
@@ -15,12 +15,16 @@ module.exports = {
 
 		const member = message.guild.members.cache.get(mention.id);
 		if (!member) return "That member isn't in the server";
+
+		args.shift();
 		member.ban(args.join(" ")).then((kicked) => {
 			message.channel.send(
 				new MessageEmbed()
 					.setColor("RED")
 					.setTitle("Banned")
-					.setDescription(`${kicked.user.tag} was banned from the server.`)
+					.setDescription(
+						`${kicked.user.tag} was banned from the server.\nReason: ${args.join(" ")}`
+					)
 					.setFooter(message.guild.name)
 			);
 		});
--- commands/server/kick.js
+++ commands/server/kick.js
@@ -15,12 +15,16 @@ module.exports = {
 
 		const member = message.guild.members.cache.get(mention.id);
 		if (!member) return "That member isn't in the server";
+
+		args.shift();
 		member.kick(args.join(" ")).then((kicked) => {
 			message.channel.send(
 				new MessageEmbed()
 					.setColor("RED")
 					.setTitle("Kicked")
-					.setDescription(`${kicked.user.tag} was kicked from the server.`)
+					.setDescription(
+						`${kicked.user.tag} was kicked from the server.\nReason: ${args.join(" ")}`
+					)
 					.setFooter(message.guild.name)
 			);
 		});
