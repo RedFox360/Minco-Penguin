3e992648e9d4c09eed8908c0bc0251ae8585977b
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-06-16 09:14:06 -0700

    ubuntu usage

--- commands/returns/ubuntu.js
+++ commands/returns/ubuntu.js
@@ -1,5 +1,6 @@
 module.exports = {
 	description: "Get the ubuntu name of someone",
+	usage: "!ubuntu name <person>",
 	servers: ["827635704145772574"],
 	execute(_, args) {
 		switch (args[1].toLowerCase()) {
