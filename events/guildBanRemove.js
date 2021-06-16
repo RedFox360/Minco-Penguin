11e6101ebe567951bfb24d26ee9c01ae77d9911a
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-06-16 14:44:06 -0700

    another async

--- events/guildBanRemove.js
+++ events/guildBanRemove.js
@@ -1,5 +1,5 @@
 const serverModel = require("../models/serverSchema");
-module.exports = (_0, guild, user) => {
+module.exports = async (_0, guild, user) => {
 	const serverData = await serverModel.findOne({ serverID: guild.id });
 	if (serverData.silenceBans) return;
 	user.send(`${user.tag}, you were banned from ${guild.name}.`);
