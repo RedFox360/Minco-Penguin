8cff4bbd8a0dddbfbb90e065fb48a4a23ebd8402
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-06-16 13:21:58 -0700

    guildBanAdd in correct channel

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -3,12 +3,15 @@ const Discord = require("discord.js");
  * @param {Discord.Guild} guild
  * @param {Discord.User} user
  */
-module.exports = (_, guild, user) => {
+module.exports = async (_, guild, user) => {
+	let serverData = await serverModel.findOne({ serverID: member.guild.id });
 	let unbanEmbed = new Discord.MessageEmbed()
 		.setColor("F75853") // red
 		.setTitle("Banned")
 		.setDescription(`${user.tag} flew too close to the sun and was banned from ${guild.name}.`);
-
+	const channel = serverData.welcomeChannel
+		? client.channels.cache.get(serverData.welcomeChannel)
+		: member.guild.systemChannel;
 	guild.systemChannel.send(unbanEmbed);
 	user.send(`${user.tag}, you were unbanned from ${guild.name}.`);
 };
