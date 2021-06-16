10e6ab7b384db13c542e7d5135f1d24e01de2149
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-06-15 18:01:24 -0700

    actually SENDS the embed

--- commands/hidden/info.js
+++ commands/hidden/info.js
@@ -6,31 +6,33 @@ module.exports = {
 		const mention = message.mentions.users.first();
 		const author = mention ?? message.author;
 		const member = message.guild.members.cache.get(author.id);
-		const infoEmbed = new MessageEmbed()
-			.setAuthor(author.tag, author.avatarURL(), author.avatarURL())
-			.setColor("16A085") // darkish green
-			.addFields(
-				{
-					name: "Roles",
-					value: member.roles.cache
-						.array()
-						.map((role) => `<@&${role.id}>`)
-						.join(" "),
+		message.channel.send(
+			new MessageEmbed()
+				.setAuthor(author.tag, author.avatarURL(), author.avatarURL())
+				.setColor("16A085") // darkish green
+				.addFields(
+					{
+						name: "Roles",
+						value: member.roles.cache
+							.array()
+							.map((role) => `<@&${role.id}>`)
+							.join(" "),
 
-					inline: true,
-				},
-				{
-					name: "Created at",
-					value: format(author.createdAt),
-					inline: true,
-				},
-				{
-					name: "Joined at",
-					value: format(member.joinedAt),
-					inline: true,
-				}
-			)
-			.footer(`User ID: ${author.id}`);
+						inline: true,
+					},
+					{
+						name: "Created at",
+						value: format(author.createdAt),
+						inline: true,
+					},
+					{
+						name: "Joined at",
+						value: format(member.joinedAt),
+						inline: true,
+					}
+				)
+				.setFooter(`User ID: ${author.id}`)
+		);
 	},
 };
 
