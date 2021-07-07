00d22a385ad9709bff87608fd0bf3f40d7467219
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-07-07 09:05:34 -0700

    deleted some

--- commands/items/buy-orbs.js
+++
@@ -1,6 +0,0 @@
-const profileModel = require("../../models/profileSchema");
-module.exports = {
-	description: "Buy orbs! 10 MD = 1 orb",
-	usage: "!buy-orbs <number of orbs>",
-	async execute() {},
-};
--- commands/items/sell-orbs.js
+++
@@ -1,6 +0,0 @@
-const profileModel = require("../../models/profileSchema");
-module.exports = {
-	description: "Sell your orbs for money (1 orb = 10 MD)",
-	usage: "!sell-orbs <number of orbs>",
-	async execute() {},
-};
