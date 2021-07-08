54220965fb1ab4544b6ef7fa3dd64d4cb4e27865
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-07-08 12:11:02 -0700

    owner only

--- commands/hidden/servers.js
+++ commands/hidden/servers.js
@@ -1,7 +1,9 @@
 module.exports = {
 	description: "[ONWER ONLY] view all the servers Minco is in",
 	execute(message, _0, _1, client) {
-		let servers = client.guilds.cache.map((guild) => guild.name).join("\n");
-		message.channel.send("```\n" + servers + "\n```");
+		if (message.author.id == "724786310711214118") {
+			let servers = client.guilds.cache.map((guild) => guild.name).join("\n");
+			message.channel.send("```\n" + servers + "\n```");
+		}
 	},
 };
