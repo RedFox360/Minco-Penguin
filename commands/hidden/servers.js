5ee3343ef73fb4f9f147a02dde214d9a3967a9ff
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-07-08 12:14:08 -0700

    numbered list

--- commands/hidden/servers.js
+++ commands/hidden/servers.js
@@ -1,8 +1,13 @@
+const { Client } = require("discord.js");
 module.exports = {
 	description: "[ONWER ONLY] view all the servers Minco is in",
+	/** @param {Client} client */
 	execute(message, _0, _1, client) {
 		if (message.author.id == "724786310711214118") {
-			let servers = client.guilds.cache.map((guild) => guild.name).join("\n");
+			let servers = client.guilds.cache
+				.array()
+				.map((guild, index) => `${index + 1}. ${guild.name}`)
+				.join("\n");
 			message.channel.send("```\n" + servers + "\n```");
 		}
 	},
