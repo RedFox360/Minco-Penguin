3ae7d5db6c80670a856bebd13fdc112d9c452de4
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-07-07 20:33:59 -0700

    send message when added

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,11 +1,28 @@
-const { Guild } = require("discord.js");
+const { Guild, Client, MessageEmbed } = require("discord.js");
 const serverModel = require("../models/serverSchema");
-/** @param {Guild} guild */
-module.exports = async (_, guild) => {
+/**
+ * @param {Guild} guild
+ * @param {Client} client
+ */
+module.exports = async (client, guild) => {
 	let serverProfile = await serverModel.create({
 		serverID: guild.id,
 		bannedPeople: [],
 		prefixes: ["!", "###", "minco "],
 	});
 	serverProfile.save();
+	const fetchedLogs = await guild.fetchAuditLogs({
+		limit: 1,
+		type: "BOT_ADD",
+	});
+	const auditLog = fetchedLogs.entries.first();
+	auditLog.executor.send(
+		new MessageEmbed()
+			.setColor("GREEN")
+			.setAuthor(client.user.avatarURL(), "Minco Penguin")
+			.setTitle("Thanks for me to your server!")
+			.setDescription(
+				"My default prefix is `!`. Use `!help server` to view all the admin commands!"
+			)
+	);
 };
