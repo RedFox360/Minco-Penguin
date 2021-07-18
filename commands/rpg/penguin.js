c8030cf306ad137abcc710968be05e522f2fa0e9
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-07-18 12:40:40 -0700

    penguin info done

--- commands/rpg/penguin.js
+++ commands/rpg/penguin.js
@@ -4,18 +4,21 @@ module.exports = {
 	description: "Adopt a penguin!",
 	execute(message, args) {
 		if (!args.length) {
-			const adoptEmbed = new MessageEmbed().setTitle("Adopt a penguin!").setDescription(
-				`Adopting a penguin will allow you to participate in battles and other rpg features
+			const adoptEmbed = new MessageEmbed()
+				.setTitle("Adopt a penguin!")
+				.setDescription(
+					`Adopting a penguin will allow you to participate in battles and other rpg features
 
 Use !adopt <type> to adopt a penguin
 
-\`Warrior | Magician | Healer | Archer \`
-:crossed_swords:\`   50   |   35   |   30   |   40\`
-:shield:\`   30   |   50   |   30   |   25\`
-:heart:\`   45   |   35   |   70   |   35\`
-:dart:\`   25   |   30   |   20   |   50\`
+\` Warrior | Magician | Healer | Archer \`
+:crossed_swords:\`   50   |   35   |   30   |   40   \`
+:shield:\`   30   |   50   |   30   |   25   \`
+:heart:\`   45   |   35   |   70   |   35   \`
+:dart:\`   25   |   30   |   20   |   50   \`
 `
-			);
+				)
+				.setColor("#9afffc");
 			message.channel.send(adoptEmbed);
 		}
 	},
