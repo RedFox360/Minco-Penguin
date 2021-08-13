bb113a53f74dd19258a2e204654c57d584706214
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-08-12 21:20:30 -0700

    src

--- src/handlers/command_handler.js
+++ src/handlers/command_handler.js
@@ -1,11 +1,11 @@
 const { readdirSync } = require("fs");
 module.exports = (client) => {
-	const categories = readdirSync("./commands/").filter(
+	const categories = readdirSync("./src/commands/").filter(
 		(file) => !file.endsWith(".DS_Store")
 	);
 	for (const category of categories) {
-		const commandFiles = readdirSync(`./commands/${category}`).filter((file) =>
-			file.endsWith(".js")
+		const commandFiles = readdirSync(`./src/commands/${category}`).filter(
+			(file) => file.endsWith(".js")
 		);
 		for (const file of commandFiles) {
 			const command = require(`../commands/${category}/${file}`);
