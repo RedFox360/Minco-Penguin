f6da515b6730eba2bbe3c2337dacd7abd567142a
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-10-15 16:16:39 -0700

    new better server info

--- src/slashCommands/return/info.ts
+++ src/slashCommands/return/info.ts
@@ -76,27 +76,40 @@ export async function run({ interaction, server, profile }: CommandData) {
 			const voiceChannelAmount = channels.filter((channel) =>
 				channel.isVoice()
 			).size;
+			const categoryAmount = channels.filter(
+				(channel) => channel.type === "GUILD_CATEGORY"
+			).size;
 			const roleAmount = (await interaction.guild.roles.fetch()).size;
 			const totalChannelAmount = textChannelAmount + voiceChannelAmount;
+			const emojiCount = (await interaction.guild.emojis.fetch()).size;
+			const roleCount = (await interaction.guild.roles.fetch()).size;
+			const verificationLevel = (() => {
+				switch (interaction.guild.verificationLevel) {
+					case "NONE":
+						return "`None`";
+					case "LOW":
+						return "`Low`";
+					case "MEDIUM":
+						return "`Medium`";
+					case "HIGH":
+						return "`High`";
+					case "VERY_HIGH":
+						return "`Very High`";
+				}
+			})();
 			const infoEmbed = new MessageEmbed()
+				.setThumbnail(interaction.guild.iconURL())
 				.setTitle("Server Info")
 				.setColor("BLURPLE")
 				.addFields(
 					{
-						name: "Created at",
-						value: formatTime(interaction.guild.createdAt, profile.timezone),
+						name: "Name",
+						value: "`" + interaction.guild.name + "`",
 						inline: true,
 					},
 					{
-						name: "Channel amounts",
-						value: `:speech_balloon: Text channels: ${textChannelAmount}
-:microphone2: Voice channels: ${voiceChannelAmount}
-:hash: Total: ${totalChannelAmount}`,
-						inline: true,
-					},
-					{
-						name: "Member Count",
-						value: `:slight_smile: Non Bots: ${server.memberCount}`,
+						name: "ID",
+						value: "`" + interaction.guild.id + "`",
 						inline: true,
 					},
 					{
@@ -105,14 +118,64 @@ export async function run({ interaction, server, profile }: CommandData) {
 						inline: true,
 					},
 					{
-						name: "Role amount",
-						value: roleAmount.toLocaleString(),
+						name: "Created at",
+						value: formatTime(interaction.guild.createdAt, profile.timezone),
+						inline: true,
+					},
+					{
+						name: "Channel amounts",
+						value: `Text: \`${textChannelAmount}\`  |  Voice: \`${voiceChannelAmount}\`  |  Categories: \`${categoryAmount}\`  |  Total: \`${totalChannelAmount}\``,
 						inline: true,
 					},
 					{
 						name: "Server Timezone",
-						value: `${server.timezone}
+						value: `\`${server.timezone}\`
 Current time: ${dayjs().tz(server.timezone).format("MMM DD hh:mm A")}`,
+						inline: true,
+					},
+					{
+						name: "Member Count",
+						value: `Non Bots: \`${server.memberCount}\``,
+						inline: true,
+					},
+					{
+						name: "Highest Role",
+						value: interaction.guild.roles.highest.toString(),
+						inline: true,
+					},
+					{
+						name: "Role amount",
+						value: "`" + roleAmount.toLocaleString() + "`",
+						inline: true,
+					},
+					{
+						name: "Partnered",
+						value: "`" + interaction.guild.partnered + "`",
+						inline: true,
+					},
+					{
+						name: "Verified",
+						value: "`" + interaction.guild.verified + "`",
+						inline: true,
+					},
+					{
+						name: "Emoji Count",
+						value: "`" + emojiCount + "`",
+						inline: true,
+					},
+					{
+						name: "Role Count",
+						value: "`" + roleCount + "`",
+						inline: true,
+					},
+					{
+						name: "System Channel",
+						value: (interaction.guild.systemChannel ?? "`None`").toString(),
+						inline: true,
+					},
+					{
+						name: "Verification Level",
+						value: verificationLevel,
 						inline: true,
 					}
 				)
@@ -136,13 +199,11 @@ const formatTime = (time, timezone) => {
 	const currentTime = Date.now();
 	const creationTime = dayjs.tz(time, timezone);
 	const timeBetween = prettyMs(currentTime - creationTime.valueOf(), {
-		unitCount: 3,
+		unitCount: 2,
 		verbose: true,
 	});
 
-	return `${formatDate(creationTime)}
-*${timeBetween}* ago`;
+	return `${formatDate(creationTime)} (*${timeBetween}* ago)`;
 };
 
-const formatDate = (date: dayjs.Dayjs) =>
-	date.format("ddd [**]MMM D, YYYY[**] hh:mm A");
+const formatDate = (date: dayjs.Dayjs) => date.format("MMM DD, YYYY h:mm A");
