7b60d45e6bb58309fa9d5f53f66e990176cf00a7
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-11-27 17:35:18 -0800

    filter files

--- src/handlers/slash_handler.ts
+++ src/handlers/slash_handler.ts
@@ -4,8 +4,8 @@ import { Client } from "discord.js";
 import { readdirSync } from "fs";
 
 export default async (client: Client) => {
-	const categories = readdirSync("./src/slashCommands/").filter(
-		(file) => !file.includes(".")
+	const categories = readdirSync("./src/slashCommands/").filter((file) =>
+		file.endsWith(".ts")
 	);
 	const data = [];
 	const dmusdOnlyData = [];
