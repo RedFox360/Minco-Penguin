62805e1f54a21dbe5b329f92feea6b7840ab487b
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2022-02-02 16:04:10 -0800

    user send fix

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,26 +1,29 @@
-import { Guild, Client, MessageEmbed } from "discord.js";
-import serverModel from "../models/serverSchema";
+import { Guild, Client, MessageEmbed } from 'discord.js';
+import serverModel from '../models/serverSchema';
 export default async (guild: Guild, client: Client) => {
 	let members = await guild.members.fetch();
 	let memberCount = members.filter((member) => !member.user.bot).size;
 	await serverModel.create({
 		serverID: guild.id,
 		bannedPeople: [],
-		prefixes: ["!", "###", "minco "],
-		memberCount,
+		prefixes: ['!', '###', 'minco '],
+		memberCount
 	});
 	const fetchedLogs = await guild.fetchAuditLogs({
 		limit: 1,
-		type: "BOT_ADD",
+		type: 'BOT_ADD'
 	});
-	fetchedLogs.entries.first().executor.send({
-		embeds: [
-			new MessageEmbed()
-				.setColor("GREEN")
-				.setTitle("Thanks for me to your server!")
-				.setDescription(
-					"Minco Penguin now has slash commands! Try using them by typing a slash (/)"
-				),
-		],
-	});
+	fetchedLogs.entries
+		.first()
+		.executor.send({
+			embeds: [
+				new MessageEmbed()
+					.setColor('GREEN')
+					.setTitle('Thanks for me to your server!')
+					.setDescription(
+						'Minco Penguin now has slash commands! Try using them by typing a slash (/)'
+					)
+			]
+		})
+		.catch(() => {});
 };
--- src/functions/market/market_buy.ts
+++ src/functions/market/market_buy.ts
@@ -1,81 +1,95 @@
-import { CommandData } from "../../types";
+import { CommandData } from '../../types';
 import {
 	MessageButton,
 	MessageActionRow,
-	MessageComponentInteraction,
-} from "discord.js";
+	MessageComponentInteraction
+} from 'discord.js';
 
 export default async function run({
 	interaction,
 	profileInServerOf,
 	profile,
-	updateProfile,
+	updateProfile
 }: CommandData) {
-	const user = interaction.options.getUser("user");
-	const itemName = interaction.options.getString("item_name");
+	const user = interaction.options.getUser('user');
+	const itemName = interaction.options.getString('item_name');
 	const uprofile = await profileInServerOf(user.id);
 	const item = uprofile.market.find((m) => m.name === itemName);
 	if (!item) {
 		await interaction.reply({
 			content: `${user.toString()} does not have that item! (remember capitalization)`,
-			ephemeral: true,
+			ephemeral: true
 		});
 		return;
 	}
 	if (profile.mincoDollars < item.price) {
 		await interaction.reply({
 			content: `You don't have ${item.price} MD (the price of the item)`,
-			ephemeral: true,
+			ephemeral: true
 		});
 		return;
 	}
 	const row = new MessageActionRow().addComponents(
 		new MessageButton()
-			.setCustomId("confirm")
-			.setLabel("Confirm")
-			.setStyle("SUCCESS"),
+			.setCustomId('confirm')
+			.setLabel('Confirm')
+			.setStyle('SUCCESS'),
 		new MessageButton()
-			.setCustomId("reject")
-			.setLabel("Nevermind")
-			.setStyle("DANGER")
+			.setCustomId('reject')
+			.setLabel('Nevermind')
+			.setStyle('DANGER')
 	);
 	const msg = await interaction.reply({
-		content: `${interaction.user.toString()}, confirm to buy ${item.name} for ${
-			item.price
-		} MD`,
+		content: `${interaction.user.toString()}, confirm to buy ${
+			item.name
+		} for ${item.price} MD`,
 		components: [row],
-		fetchReply: true,
+		fetchReply: true
 	});
 
 	const filter = (i) =>
-		(i.customId == "confirm" || i.customId == "reject") &&
+		(i.customId === 'confirm' || i.customId === 'reject') &&
 		i.user.id === interaction.user.id;
 
 	const collector = msg.createMessageComponentCollector({
 		filter,
 		time: 10_000,
-		max: 1,
+		max: 1
 	});
 	let sendTimeout = true;
-	collector.on("collect", async (i: MessageComponentInteraction) => {
+	collector.on('collect', async (i: MessageComponentInteraction) => {
 		if (!i.isMessageComponent()) return;
 		sendTimeout = false;
 		await i.deferUpdate();
-		if (i.customId == "confirm") {
+		if (i.customId === 'confirm') {
 			await updateProfile({ $inc: { mincoDollars: -item.price } });
-			await updateProfile({ $inc: { mincoDollars: item.price } }, user.id);
-			await user.send(
-				`${interaction.user.toString()} bought your **${item.name}**`
+			await updateProfile(
+				{ $inc: { mincoDollars: item.price } },
+				user.id
 			);
-			await interaction.followUp(
-				`You succesfully bought that item! ${user.toString()} will be DMed notifying your purchase.`
-			);
+			try {
+				await user.send(
+					`${interaction.user.toString()} bought your **${
+						item.name
+					}**`
+				);
+				await interaction.followUp(
+					`You succesfully bought that item! ${user.toString()} will be DMed notifying your purchase.`
+				);
+			} catch (err) {
+				await interaction.followUp(
+					`You succesfully bought that item, but ${user}'s DMS are closed.`
+				);
+			}
 		} else {
-			await interaction.followUp("Request canceled");
+			await interaction.followUp('Request canceled');
 		}
 	});
-	collector.on("end", async () => {
+	collector.on('end', async () => {
 		if (sendTimeout)
-			await interaction.followUp({ content: "Timed out!", ephemeral: true });
+			await interaction.followUp({
+				content: 'Timed out!',
+				ephemeral: true
+			});
 	});
 }
