fcfa508628b056ab933d9e77d8aad1905c675dec
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2022-02-14 15:59:52 -0800

    fixed MAJOR giveuser issue

--- src/slashCommands/economy/giveuser.ts
+++ src/slashCommands/economy/giveuser.ts
@@ -1,49 +1,57 @@
-import { CommandData } from "../../types";
-import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandData } from '../../types';
+import { SlashCommandBuilder } from '@discordjs/builders';
 
 export const data = new SlashCommandBuilder()
-	.setName("giveuser")
-	.setDescription("Give a user money")
-	.addUserOption((option) =>
+	.setName('giveuser')
+	.setDescription('Give a user money')
+	.addUserOption(option =>
 		option
-			.setName("user")
-			.setDescription("The user to give money to")
+			.setName('user')
+			.setDescription('The user to give money to')
 			.setRequired(true)
 	)
-	.addIntegerOption((option) =>
+	.addIntegerOption(option =>
 		option
-			.setName("amount")
-			.setDescription("The amount of money to give")
+			.setName('amount')
+			.setDescription('The amount of money to give')
 			.setRequired(true)
 	);
 
-export async function run({ interaction, updateProfile }: CommandData) {
+export async function run({
+	interaction,
+	updateProfile
+}: CommandData) {
 	if (
-		!["724786310711214118", "802668636795830292"].includes(interaction.user.id)
+		!['724786310711214118', '802668636795830292'].includes(
+			interaction.user.id
+		)
 	) {
 		await interaction.reply({
-			content: "This command can only be used by the owner",
-			ephemeral: true,
+			content: 'This command can only be used by the owner',
+			ephemeral: true
 		});
+		return;
 	}
-	const user = interaction.options.getUser("user");
-	const amount = interaction.options.getInteger("amount");
-	if (interaction.user.id === "802668636795830292") {
+	const user = interaction.options.getUser('user');
+	const amount = interaction.options.getInteger('amount');
+	if (interaction.user.id === '802668636795830292') {
 		if (amount < 100) {
 			await interaction.reply({
 				content: "You can't give more than 100 MD",
-				ephemeral: true,
+				ephemeral: true
 			});
 			return;
 		}
-		if (user.id == "802668636795830292") {
+		if (user.id === '802668636795830292') {
 			await interaction.reply({
 				content: "You can't give money to yourself",
-				ephemeral: true,
+				ephemeral: true
 			});
 			return;
 		}
 	}
 	await updateProfile({ $inc: { mincoDollars: amount } }, user.id);
-	interaction.reply(`You added ${amount} MD to ${user.toString()}'s profile`);
+	interaction.reply(
+		`You added ${amount} MD to ${user.toString()}'s profile`
+	);
 }
