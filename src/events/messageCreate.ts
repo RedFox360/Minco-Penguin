edaf58fd99e120d2083a4d7f2c96a50b3dc21d63
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2022-03-05 17:48:27 -0800

    Added word to whitelist

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,46 +1,46 @@
-import { Message } from "discord.js";
-import serverModel from "../models/serverSchema";
-import { ServerData } from "../types";
-import filter from "leo-profanity";
-import profileInServerModel from "../models/profileInServerSchema";
+import { Message } from 'discord.js';
+import serverModel from '../models/serverSchema';
+import { ServerData } from '../types';
+import filter from 'leo-profanity';
+import profileInServerModel from '../models/profileInServerSchema';
 filter.add([
-	"fucked",
-	"fuced",
-	"fuc",
-	"fuq",
-	"stfu",
-	"feck",
-	"fawk",
-	"shet",
-	"betch",
-	"cocaine",
-	"heroin",
-	"retard",
-	"retarded",
+	'fucked',
+	'fuced',
+	'fuc',
+	'fuq',
+	'stfu',
+	'feck',
+	'fawk',
+	'shet',
+	'betch',
+	'cocaine',
+	'heroin',
+	'retard',
+	'retarded'
 ]);
-filter.remove(["suck", "sucks", "butt"]);
+filter.remove(['suck', 'sucks', 'butt', 'escort']);
 
 export default async (message: Message) => {
 	if (!message.guild || !message.author) return;
-	if (!message.guild.me.permissions.has("MANAGE_MESSAGES")) return;
+	if (!message.guild.me.permissions.has('MANAGE_MESSAGES')) return;
 	const server: ServerData = await serverModel.findOne({
-		serverID: message.guild.id,
+		serverID: message.guild.id
 	});
 	if (
 		server.clean &&
 		(filter.check(
 			message.content
-				.replaceAll(/[!@#$%^&*()-+/.,;'~`=_><?{}]/g, "")
-				.replaceAll("\n", " ")
+				.replaceAll(/[!@#$%^&*()-+/.,;'~`=_><?{}]/g, '')
+				.replaceAll('\n', ' ')
 		) ||
-			message.content.includes("middle_finger"))
+			message.content.includes('middle_finger'))
 	) {
 		await message.delete();
 		return;
 	}
 	const profile = await profileInServerModel.findOne({
 		userID: message.author.id,
-		serverID: message.guild.id,
+		serverID: message.guild.id
 	});
 	if (profile?.isShadowBanned) {
 		await message.delete();
--- src/handlers/slash_handler.ts
+++ src/handlers/slash_handler.ts
@@ -1,39 +1,41 @@
-import { rest } from "../main";
-import { Routes } from "discord-api-types/v9";
-import { Client } from "discord.js";
-import { readdirSync } from "fs";
+import { rest } from '../main';
+import { Routes } from 'discord-api-types/v9';
+import { Client } from 'discord.js';
+import { readdirSync } from 'fs';
 
 export default async (client: Client, updateCommands: boolean) => {
-	const categories = readdirSync("./src/slashCommands/").filter(
-		(file) => !file.includes(".")
+	const categories = readdirSync('./src/slashCommands/').filter(
+		file => !file.includes('.')
 	);
 	const data = [];
 	const dmusdOnlyData = [];
 	for (const category of categories) {
-		const commands = readdirSync(`./src/slashCommands/${category}`).filter(
-			(file) => file.endsWith(".ts")
-		);
+		const commands = readdirSync(
+			`./src/slashCommands/${category}`
+		).filter(file => file.endsWith('.ts'));
 		for (const commandName of commands) {
 			const command = await import(
 				`../slashCommands/${category}/${commandName}`
 			);
 			const commandData = command.data.toJSON();
-			if (category === "dmusd_only") {
+			if (category === 'dmusd_only') {
 				dmusdOnlyData.push(commandData);
 			} else {
 				data.push(commandData);
 			}
 			(client as any).commands.set(commandData.name, command);
-			console.log("added " + commandData.name);
+			console.log('added ' + commandData.name);
 		}
 	}
-	console.log(`commands added || command count: ${data.length + 1}`);
+	console.log(
+		`commands added || command count: ${data.length + 1}`
+	);
 	if (updateCommands) {
 		await rest.put(Routes.applicationCommands(client.user.id), {
-			body: data,
+			body: data
 		});
 		await client.guilds.cache
-			.get("785642761814671381")
+			.get('785642761814671381')
 			?.commands.set(dmusdOnlyData);
 	}
 };
